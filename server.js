423ed762509ca969983e68fe6b64bdd9b878a96e
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-10 10:27:31 -0700

    staging for jitsi

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var app = express();
 const hostname = 'anomic.io';
 const port = 443;
 var server = https.createServer({
+cert: fs.readFileSync('./config/keys/anomic_io.crt'),
+ca: fs.readFileSync('./config/keys/anomic_io.ca-bundle'),
+key: fs.readFileSync('./config/keys/private.key'),
 },app);
 server.listen(443);
 
