d52aaafc42a9688bf1f6a7d03270ebac69f68648
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-13 10:46:23 -0700

    janusapi

--- routes/index.js
+++ routes/index.js
@@ -48,15 +48,15 @@ router.post('/logout', (req, res) => {
   })
 })
 
-const Janus = require('/src/Janus')
-const JanusAdmin = require('/src/JanusAdmin')
-const JanusPlugin = require('/src/JanusPlugin')
-const EchoJanusPlugin = require('/src/plugin/EchoJanusPlugin')
-const RecordPlayJanusPlugin = require('/src/plugin/RecordPlayJanusPlugin')
-const VideoRoomListenerJanusPlugin = require('/src/plugin/VideoRoomListenerJanusPlugin')
-const VideoRoomPublisherJanusPlugin = require('/src/plugin/VideoRoomPublisherJanusPlugin')
-const StreamingJanusPlugin = require('/src/plugin/StreamingJanusPlugin')
-const { JanusConfig, JanusAdminConfig, JanusRoomConfig } = require('/src/Config')
+const Janus = require('../src/Janus')
+const JanusAdmin = require('../src/JanusAdmin')
+const JanusPlugin = require('../src/JanusPlugin')
+const EchoJanusPlugin = require('../src/plugin/EchoJanusPlugin')
+const RecordPlayJanusPlugin = require('../src/plugin/RecordPlayJanusPlugin')
+const VideoRoomListenerJanusPlugin = require('../src/plugin/VideoRoomListenerJanusPlugin')
+const VideoRoomPublisherJanusPlugin = require('../src/plugin/VideoRoomPublisherJanusPlugin')
+const StreamingJanusPlugin = require('../src/plugin/StreamingJanusPlugin')
+const { JanusConfig, JanusAdminConfig, JanusRoomConfig } = require('../src/Config')
 
 module.exports = {
   Janus,
