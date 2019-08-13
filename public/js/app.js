b56f09aa665ca9e074250b53d72009cb9533690e
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-13 06:06:15 -0700

    webrtcinternals

--- public/js/app.js
+++ public/js/app.js
@@ -163,6 +163,308 @@ var socket = io.connect('anomic.io/');
 
   // Register new Chat Room
 
+  function joinSDP()
+  {
+      var sdpStatic =
+          %$SDP_OFFER$%
+      ;
+      
+      var sdpDeprecated = "v=0\n" +
+  "o=mozilla...THIS_IS_SDPARTA-38.0.1_cookie%$AUTHCOOKIE$% 1702670192771025677 0 IN IP4 0.0.0.0\n" + 
+  "s=-\n" + 
+  "t=0 0\n" + 
+  "a=fingerprint:sha-256 5C:FF:65:F6:7E:39:38:E6:CF:49:08:E5:73:2C:93:0E:59:13:24:23:22:37:10:50:6E:F1:9E:4A:45:DB:25:F4\n" + 
+  "a=group:BUNDLE sdparta_0 sdparta_1\n" + 
+  "a=ice-options:trickle\n" + 
+  "a=msid-semantic:WMS *\n" + 
+  "m=audio 9 RTP/SAVPF 109 9 0 8\n" + 
+  "c=IN IP4 0.0.0.0\n" + 
+  "a=sendrecv\n" + 
+  "a=extmap:1 urn:ietf:params:rtp-hdrext:ssrc-audio-level\n" + 
+  "a=ice-pwd:230r89wef32jsdsjJlkj23rndasf23rlknas\n" +
+  "a=ice-ufrag:aaaaaaaa\n" +
+  "a=mid:sdparta_0\n" + 
+  "a=msid:{7e5b1422-7cbe-3649-9897-864febd59342} {6fca7dee-f59d-3c4f-be9c-8dd1092b10e3}\n" + 
+  "a=rtcp-mux\n" + 
+  "a=rtpmap:109 opus/48000/2\n" + 
+  "a=rtpmap:9 G722/8000/1\n" + 
+  "a=rtpmap:0 PCMU/8000\n" + 
+  "a=rtpmap:8 PCMA/8000\n" + 
+  "a=setup:actpass\n" +
+  /*"744719343"*/
+  "a=ssrc:%$SDP_SSRC1$%"+" cname:{5f2c7e38-d761-f64c-91f4-682ab07ec727}\n" +
+  "m=video 9 RTP/SAVPF 120 126 97\n" + 
+  "c=IN IP4 0.0.0.0\n" + 
+  "a=sendrecv\n" + 
+  /*
+  "a=fmtp:120 max-fs=12288;max-fr=60\n" + 
+  */
+  "a=fmtp:120 max-fs=450;max-fr=60\n" + 
+  "a=fmtp:126 profile-level-id=42e01f;level-asymmetry-allowed=1;packetization-mode=1\n" + 
+  "a=fmtp:97 profile-level-id=42e01f;level-asymmetry-allowed=1\n" + 
+  "a=ice-pwd:230r89wef32jsdsjJlkj23rndasf23rlknas\n" +
+  "a=ice-ufrag:aaaaaaaa\n" +
+  "a=mid:sdparta_1\n" + 
+  "a=msid:{7e5b1422-7cbe-3649-9897-864febd59342} {f46f496f-30aa-bd40-8746-47bda9150d23}\n" + 
+  "a=rtcp-fb:120" +
+  " ccm" + 
+  " fir" +
+  " pli" +
+  " nack" +
+  "\n" + 
+  /*
+  "a=rtcp-fb:120 ccm fir\n" + 
+  */
+  /*
+  "a=rtcp-fb:126 nack\n" + 
+  "a=rtcp-fb:126 nack pli\n" + 
+  */
+  "a=rtcp-fb:126 ccm fir\n" + 
+  /*
+  "a=rtcp-fb:97 nack\n" + 
+  "a=rtcp-fb:97 nack pli\n" + 
+  */
+  "a=rtcp-fb:97 ccm fir\n" + 
+  "a=rtcp-mux\n" + 
+  "a=rtpmap:120 VP8/90000\n" +
+  "a=rtpmap:126 H264/90000\n" +
+  "a=rtpmap:97 H264/90000\n" +
+  "a=setup:actpass\n" +
+  /*"790737109"*/
+  "a=ssrc:%$SDP_SSRC2$%"+" cname:{5f2c7e38-d761-f64c-91f4-682ab07ec727}\n"
+  ;
+    return sdpStatic;
+  }
+
+
+  var remoteConnection = null;
+  var remoteConnectionOffer = null;
+  var remoteConnectionAnswer = null;
+  var remoteConnectionLocalDescription = null;
+  var remoteVideo = null;
+  var localConnection = null;
+  var localStream = null;
+  var iceCandidate = null;
+  var iceCandidateID = 0;
+  var remoteConnectionStunConfig = %$STUNCONFIGJS$%;
+  var closeHandler = null;
+  //var stunHost = "%$HOSTNAME$%";
+  var stunHost = document.location.host;
+  //var stunPort = "3478";
+  var stunPort = "%$RTPPORT$%";
+  //var onLoadDoneAnswerUpload;
+
+  function onPeerClick(peername, elem) {
+      document.theform.peerstream_recv.value = peername;
+      document.theform.appendsdp.value = 'a=watch=' + peername + '\n';
+      document.theform.appendsdp.value += 'a=myname=' + document.theform.my_name.value + '\n';
+  }
+
+  function onIceCandidate(event) {
+      console.debug("onIceCandidate");
+
+      if (event.candidate && iceCandidate == null) {
+          //var c = event.candidate;
+          var c = {};
+          console.debug("onIceCandidate.event.candidate:"+JSON.stringify(event.candidate));
+
+          iceCandidate = event.candidate;
+          if(remoteConnectionStunConfig == null) {
+              //c.candidate = "candidate:" + iceCandidateID++ + " 1 UDP " + 1234+iceCandidateID + " " + stunHost + " " + stunPort + " typ host";
+              c.candidate = "%$RTCICECANDIDATE$%";
+              c.sdpMid = event.candidate.sdpMid;
+              c.sdpMLineIndex = event.candidate.sdpMLineIndex;
+              c.usernameFragment = event.candidate.usernameFragment;
+              //alert(c.candidate);
+              console.debug("onIceCandidate:"+JSON.stringify(c.candidate));
+          }
+          //alert('iceCandidate:' + c.candidate);
+          remoteConnection.addIceCandidate(
+              new RTCIceCandidate(c)).then(
+                  _ => {
+                      console.debug("onIceCandidate.then");
+                  }).catch(
+                  e => {
+                      console.debug("error in addIceCandidate");
+                  });
+      }
+  }
+
+  function onConnect() {
+      //var servers = {"iceServers": [{"url": "stun:"+stunHost+":"+stunPort}]};
+      var servers = null;
+      //remoteConnection = new RTCPeerConnection(servers);
+
+      // TODO: reorder this so that form can submit SDP prior to STUN/ICE starting
+      remoteConnection.onicecandidate = onIceCandidate;
+
+      remoteConnection.onaddstream = function(e) {
+          //alert('onAddStream' + e.stream);
+          console.debug('remoteConnection.ontrack');
+      };
+
+      /* optionally set local description (send) */
+      if(!document.theform.recvonly.checked) {
+          //remoteConnection.addStream(localStream);
+
+          localStream.getTracks().forEach(track => remoteConnection.addTrack(track, localStream));
+      }
+
+      remoteConnectionOffer = new RTCSessionDescription({type: 'offer', sdp: document.theform.offersdp.value});
+
+      remoteConnection.setRemoteDescription(remoteConnectionOffer).then(
+          function () {
+              console.debug('setRemoteScription.then');
+
+              remoteConnection.createAnswer({'mandatory': {'OfferToReceiveAudio': true, 'OfferToReceiveVideo': true}}).then(
+                  function (answer) {
+                      remoteConnectionAnswer = answer;
+                      document.theform.answersdp.value = answer.sdp;
+                      doSubmit();
+
+                      // moved remoteConnection.setLocalDescription() to broadcastStart()
+
+                  }).catch( function (err) { alert('createAnswer fail:'+err); });
+          }).catch(
+              function (err) {
+                  alert('setRemoteDescription fail: '+err);
+              }
+          );
+
+      //localConnection = new RTCPeerConnection(servers);
+
+  }
+
+  function broadcastStart(onSuccess, onFailure) {
+      var remoteStream = remoteConnection.getRemoteStreams()[0];
+
+      remoteConnection.setLocalDescription(remoteConnectionAnswer).then(
+          function () {
+              console.debug('remoteConnection.setLocalDescription');
+              if(remoteStream == null) {
+                  console.debug('remoteConnection has no streams (sendonly?)');
+                  onSuccess();
+                  return;
+              }
+              //attachMediaStream(remoteVideo, remoteConnection.getRemoteStreams()[0]);
+              //remoteStream.getTracks().forEach(track => remoteConnection.addTrack(track, remoteStream));
+              onSuccess();
+          }).catch(
+              function (err) {
+              console.debug('remoteConnection.setLocalDescription error:'+err);
+              onFailure();
+          }
+      );
+  }
+
+  function doSubmit() {
+      var appsdp = document.theform.appendsdp;
+      var myname = document.theform.my_name;
+      if(appsdp.value.indexOf('a=watch=') < 0) {
+          console.debug('no a=watch found, adding one');
+          appsdp.value += 'a=watch=' + myname.value;
+      }
+      document.finalform.answersdp.value = document.theform.answersdp.value + '\n' + document.theform.appendsdp.value;
+      document.finalform.target = 'iframe_submit';
+      document.finalform.submit();
+  }
+
+  function iframeOnLoad() {
+      broadcastStart(
+          function() {
+              closeHandler(remoteConnection, document.theform.my_name.value, document.theform.recvonly.checked, document.theform.room_name.value);
+          },
+          function() {
+              alert('broadcastStart failed');
+          }
+      );
+  }
+
+  function rtcPopupCreate(handlerOpen, handlerClose, recvOnly, watchUser) {
+      var randomNum = Math.ceil(Math.random() % 10 * 1000);
+      var w = window.open('answer_upload.html?name='+watchUser, 'sdp_answer_upload' + randomNum, '');
+      popupRecvOnly = recvOnly;
+      //w.document.body.onload = handlerOpen1;
+      onLoadDoneAnswerUpload = handlerOpen;
+      closeHandler = handlerClose;
+
+      return w;
+  }
+
+  function rtcPopupCreateIframe(handlerOpen, handlerClose) {
+      document.location = 'answer_upload.html';
+      popupRecvOnly = false;
+      //w.document.body.onload = handlerOpen1;
+      console.debug('rtcPopupCreateIframe');
+      parent.onLoadDoneAnswerUpload = handlerOpen;
+      closeHandler = handlerClose;
+  }
+
+  function roomlistPopupCreate(roomName) {
+      var w = window.open('room.html?room='+roomName, 'room' + roomName, 'width=250,height=300');
+  }
+
+  function resizeObjectWithID(idName, x, y, w, h) {
+      var d = document.getElementById(idName);
+      if(d) { 
+          d.style.cssText = 'position:fixed; top:'+y.toString()+'px; left:'+x.toString()+'px; width:'+w.toString()+'px; height:'+h.toString()+'px;';
+      }
+  }
+
+  function attachMediaStream(vidElem, vidStream)
+  {
+      var cssButton = 'width:32px; height:32px; position:relative; top:-50px; background-position:center; background-repeat:no-repeat;';
+      if(vidElem.srcObject != null) {
+          console.debug('attachMediaStream: video element srcObject != null, ignoring');
+          return;
+      }
+
+      vidElem.srcObject = vidStream;
+      vidElem.onloadedmetadata = function() {
+
+          if(vidElem.startButton != null) { return; }
+
+          var startButton = document.createElement('button');
+
+          startButton.onclick = function() {
+              if(vidElem.muted) {
+                vidElem.controls = true;
+                vidElem.muted = false;
+                vidElem.startButton.style.cssText = cssButton + ' background-image:url(/content/img/stop.png); z-index:1;';
+              }
+              else {
+                vidElem.muted = true;
+                if(vidElem.closeAction) {
+                    vidElem.closeAction();
+                }
+              }
+          }
+          vidElem.startButton = startButton;
+
+          vidElem.onended = function() {
+              vidElem.onended = null;
+              console.debug('vidElem.onended');
+
+              vidElem.controls = false;
+              if(vidElem.srcObject) {
+                  // commented this out since it kills local streams and are unrecoverable
+                  vidElem.srcObject.getTracks().forEach(track=>track.stop());
+                  vidElem.srcObject = null;
+              }
+              if(!vidElem.startButton) return;
+
+              vidElem.startButton.parentNode.removeChild(vidElem.startButton);
+              vidElem.startButton = null;
+          }
+
+          startButton.style.cssText = cssButton + ' background-image:url(/content/img/unmute.png); z-index:1;';
+          vidElem.parentNode.appendChild(startButton);
+
+          console.debug('attachMediaStream: onloadedmetadata');
+      }
+  }
+
+
 
   // Join existing Chat Room
 //  const joinRoom = (roomName) => {
