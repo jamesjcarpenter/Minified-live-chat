c8eabf119f9431e72a4f763efb9369a6d5a1b513
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-14 10:33:41 -0700

    testing room functionalities

--- public/js/videoroomtest.js
+++ public/js/videoroomtest.js
@@ -20,7 +20,7 @@
 // If you want to use the WebSockets frontend to Janus, instead, you'll
 // have to pass a different kind of address, e.g.:
 //
-		var server = "wss://" + window.location.hostname + ":8088";
+var server = "https://" + window.location.hostname + ":8088/janus";
 //
 // Of course this assumes that support for WebSockets has been built in
 // when compiling the server. WebSockets support has not been tested
