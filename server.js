5268dc3c9fc7c4a47ba55076dd06bb17dfeeddbe
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-14 12:09:21 -0700

    testing room functionalities

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express();
 const hostname = 'anomic.io';
 const port = 443;
-var server = "janus";
+var server = "/janus";
 
 //make sure you keep this order
 var io = require('socket.io').listen(https);
