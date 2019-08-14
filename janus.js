7cefd0422dd8b597a28d9b55ef678155fa2d5260
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-14 10:44:12 -0700

    testing room functionalities

--- janus.js
+++ janus.js
@@ -25,7 +25,7 @@
 // List of sessions
 Janus.sessions = {};
 
-var server = "https://" + window.location.hostname + ":8088/janus";
+var server = "https://" + window.location.hostname + ":8088/";
 
 Janus.isExtensionEnabled = function() {
 	if(navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
