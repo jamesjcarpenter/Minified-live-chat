bc42d140fc5cc07f90e6e8e06cf820973247efc9
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-14 10:53:56 -0700

    testing room functionalities

--- janus.js
+++ janus.js
@@ -25,7 +25,7 @@
 // List of sessions
 Janus.sessions = {};
 
-var server = "https://www.example.com:8088/janus";
+var server = "https://anomic.io:8088/janus";
 
 Janus.isExtensionEnabled = function() {
 	if(navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
