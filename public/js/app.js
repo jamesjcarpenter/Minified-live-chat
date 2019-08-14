263c50bc2aa55bd15f71ddaf8c57a174742741b8
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-14 15:34:22 -0700

    webrtc html changes

--- public/js/app.js
+++ public/js/app.js
@@ -17,40 +17,6 @@ var socket = io.connect('anomic.io/');
   // Add validation rules to Create/Join Room Form
 
   // create our webrtc connection
-  const webrtc = new SimpleWebRTC({
-    // the id/element dom element that will hold "our" video
-    localVideoEl: 'local-video',
-    // the id/element dom element that will hold remote videos
-    remoteVideosEl: 'remote-videos',
-    // immediately ask for camera access
-    autoRequestMedia: true,
-    debug: false,
-    detectSpeakingEvents: true,
-    autoAdjustMic: false,
-  });
-
-  // We got access to local camera
-  webrtc.on('localStream', () => {
-    localImageEl.hide();
-    localVideoEl.show();
-  //  webrtc.joinRoom(roomName);
-  //  postMessage(`${username} joined chatroom`);
-  });
-
-  // Remote video was added
-  webrtc.on('videoAdded', (video, peer) => {
-    // eslint-disable-next-line no-console
-    const id = webrtc.getDomId(peer);
-    const html = remoteVideoTemplate({ id });
-    if (remoteVideosCount === 0) {
-      remoteVideosEl.html(html);
-    } else {
-      remoteVideosEl.append(html);
-    }
-    $(`#${id}`).html(video);
-    $(`#${id} video`).addClass('ui image medium'); // Make video element responsive
-    remoteVideosCount += 1;
-  });
 
   // listener, whenever the server emits 'updaterooms', this updates the room the client is in
   socket.on('updaterooms', function(rooms, current_room) {
