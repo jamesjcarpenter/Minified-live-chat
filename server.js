3047cb85581c15c64b93dcae3b84291a122847d3
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-20 15:39:25 -0700

    changes to chat

--- server.js
+++ server.js
@@ -218,9 +218,9 @@ io.sockets.on('connection', function (socket) {
 	socket.on('adduser', function(req, res){
 		// store the username in the socket session for this client
 		// store the room name in the socket session for this client
-    socket.username = username;
+    socket.username = user.name;
 		// add the client's username to the global list
-		usernames[username] = username;
+		usernames[username] = user.name;
 		// echo to client they've connected
 		socket.emit('updatechat', 'SERVER', 'you have connected');
     
