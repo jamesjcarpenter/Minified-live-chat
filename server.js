93b9d7de72b137c44e9f8e16fde9b02c03e929c2
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-21 06:11:16 -0700

    changing room detail

--- server.js
+++ server.js
@@ -214,17 +214,18 @@ io.sockets.on('connection', function (socket) {
 
 	// when the client emits 'adduser', this listens and executes
 	socket.on('adduser', function(username){
-    socket.username = username;
-    usernames[username] = username;
 		// store the username in the socket session for this client
+		socket.username = username;
 		// store the room name in the socket session for this client
 		socket.room = 'room1';
+		// add the client's username to the global list
+		usernames[username] = username;
 		// send client to room 1
 		socket.join('room1');
 		// echo to client they've connected
-		io.emit('updatechat', 'SERVER', 'connected to room1');
+		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
 		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('room1').emit('updatechat', 'SERVER', user.name + ' has connected to this room');
+		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
 		socket.emit('updaterooms', rooms, 'room1');
 	});
 
