9406f732e19e8cafa7feba1aff7945bd00909ae3
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-20 22:22:07 -0700

    changing room detail

--- models/roomschema.js
+++ models/roomschema.js
@@ -3,8 +3,8 @@ const express = require('express')
 const session = require('express-session')
 
 const RoomSchema = new mongoose.Schema({
-  name1: { type: String, default: "", required: true },
-   name2: { type: String, default: "", required: true },
+  name1: { type: String, default: "", },
+   name2: { type: String, default: "",},
    members: [],
    lastActive: { type: Date, default: Date.now },
    createdOn: { type: Date, default: Date.now }
