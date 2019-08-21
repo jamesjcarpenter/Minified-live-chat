cb3e31dbcb265dcb30fcc3f59aafeb571e98c439
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-21 12:49:05 -0700

    theme change

--- server.js
+++ server.js
@@ -75,25 +75,30 @@ mongoose.Promise = global.Promise;
 
 app.use(cors())
 
-const { RateLimiterMemory } = require('rate-limiter-flexible');
-const rateLimiter = new RateLimiterMemory(
-  {
-    points: 3, // 5 points
-    duration: 1, // per second
-  });
+const { RateLimiterClusterMaster, RateLimiterCluster } = require('rate-limiter-flexible');
+if (cluster.isMaster) {
+  // Doesn't require any options, it is only storage and messages handler
+  new RateLimiterClusterMaster();
 
-  io.on('connection', (socket) => {
-    socket.on('bcast', async (data) => {
-      try {
-        await rateLimiter.consume(socket.handshake.address); // consume 1 point per event from IP
-        socket.emit('news', { 'data': data });
-        socket.broadcast.emit('news', { 'data': data });
-      } catch(rejRes) {
-        res.status(429).send('Too Many Requests');
-        socket.emit('blocked', { 'retry-ms': rejRes.msBeforeNext });
-      }
-    });
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  const rateLimiter = new RateLimiterCluster({
+    keyPrefix: 'myclusterlimiter', // Must be unique for each limiter
+    points: 100,
+    duration: 1,
+    timeoutMs: 3000 // Promise is rejected, if master doesn't answer for 3 secs
   });
+  
+  rateLimiter.consume(remoteAddress, 2) // consume 2 points
+    .then((rateLimiterRes) => {
+      // 2 points consumed
+    })
+    .catch((rateLimiterRes) => {
+      // Not enough points to consume
+    });
+}  
 
 var corsOptions = {
   origin: 'https://anomic.io',
