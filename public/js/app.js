950db2b3513588ce2d31005897f7efbf9371fcd1
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-20 17:53:44 -0700

    usermodel

--- public/js/app.js
+++ public/js/app.js
@@ -7,47 +7,7 @@ window.addEventListener('load', () => {
 var socket = io.connect('anomic.io/');
 
   // Add validation rules to Create/Join Room Form
-  socket.on('connect', function(){
-		// call the server-side function 'adduser' and send one parameter (value of prompt)
-		socket.emit('adduser', prompt("What's your name?"));
-	});
 
-	// listener, whenever the server emits 'updatechat', this updates the chat body
-	// listener, whenever the server emits 'updaterooms', this updates the room the client is in
-	socket.on('updaterooms', function(rooms, current_room) {
-		$('#rooms').empty();
-		$.each(rooms, function(key, value) {
-			if(value == current_room){
-				$('#rooms').append('<div>' + value + '</div>');
-			}
-			else {
-				$('#rooms').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
-			}
-		});
-	});
-
-	function switchRoom(room){
-		socket.emit('switchRoom', room);
-	}
-
-	// on load of page
-	$(function(){
-		// when the client clicks SEND
-		$('#datasend').click( function() {
-			var message = $('#data').val();
-			$('#data').val('');
-			// tell server to execute 'sendchat' and send along one parameter
-			socket.emit('sendchat', message);
-		});
-
-		// when the client hits ENTER on their keyboard
-		$('#data').keypress(function(e) {
-			if(e.which == 13) {
-				$(this).blur();
-				$('#datasend').focus().click();
-			}
-		});
-	});
   // create our webrtc connection
 
   // listener, whenever the server emits 'updaterooms', this updates the room the client is in
