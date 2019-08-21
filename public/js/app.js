5175dfa9cc66bcf1c7223830b3d62b3f95d477f1
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-21 06:56:52 -0700

    changing room detail

--- public/js/app.js
+++ public/js/app.js
@@ -9,22 +9,8 @@ var socket = io.connect('anomic.io/');
   // Add validation rules to Create/Join Room Form
   socket.on('connect', function(){
 		// call the server-side function 'adduser' and send one parameter (value of prompt)
-    socket.emit('adduser',
-    $('.ui.modal')
-    .modal({  
-      blurring: true,
-      closable  : false,
-      onDeny    : function(){
-        window.alert('Wait not yet!');
-        return false;
-      },
-      onApprove : function() {
-        window.close();
-      }
-    //
-    
-    }).modal('show'));
-  });
+		socket.emit('adduser', prompt("Enter username."));
+	});
   // create our webrtc connection
   socket.on('updatechat', function (username, data) {
     $('#conversation').append('<div class="ui container"><h6>' +'<h6>' + '<div class="ui medium basic segment"><span class="ui small white text"><p>'+ '<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"><tag id="username">' + username + '</tag>' + '<samp><em>' + '</em></samp>' + '</p></span><span class="ui tiny white text"><div class="ui teal medium left pointing label"id="message">' + '</span><p><span class="ui medium white text">' + data + '</div></p></span></div></div>' + '</h6></div></div>');
