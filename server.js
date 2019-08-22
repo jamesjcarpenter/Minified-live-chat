de158077d835329c80344f3a62af216cbf4460a0
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-21 17:11:14 -0700

    theme change

--- server.js
+++ server.js
@@ -103,7 +103,6 @@ const socketAntiSpam = new SocketAntiSpam({
   kickThreshold:      2,          // User gets kicked after this many spam score
   kickTimesBeforeBan: 1,          // User gets banned after this many kicks
   banning:            true,       // Uses temp IP banning after kickTimesBeforeBan
-  io:                 io,  // Bind the socket.io variable
   redis:              client,      // Redis client if you are sharing multiple servers
 })
 
