4048c9e7767e169215713671e1f920e40585fab9
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-22 08:01:26 -0700

    theme change

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,7 @@ document.getElementById('themechanger').onclick = function () {
     $('.icon').addClass('inverted');
     $('.ui.medium.left.pointing.label').addClass('inverted');
     $('.large.ui.teal.secondary.button.inverted').removeClass('large ui teal').addClass('large ui black');
+    $('#cpybutton').removeClass('ui teal').addClass('ui black');
 };
 
 
