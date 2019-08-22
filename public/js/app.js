a2c39c488c5e9b3fa25257fbcd2a6012b6801141
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-21 17:51:05 -0700

    theme change

--- public/js/app.js
+++ public/js/app.js
@@ -32,35 +32,6 @@ var socket = io.connect('anomic.io/');
     $('#scrollable').animate({ scrollTop: 		$('#scrollable').prop('scrollHeight')}, 100);
     $("#data").focus();
     
-    var sIO = {};
-    
-    sIO.on = (function(){
-        var messages = {};
-        var speedLimit = 5; //5ms
-        return function(message, handler) {
-            messages[message] = messages[message] || {};
-            if(messages[message].timestamp && new Date().getTime() - messages[message].timestamp < speedLimit) return false;
-            else messages[message].timestamp = new Date().getTime();
-            
-            handler();
-            return true;
-            //execute code, Ex:
-        }
-    }());
-    
-    //sIO.on("hello",function(){console.log("it works");});
-    
-    //sIO.on("hi",function(){console.log("it works too");});
-    
-    var i = 0;
-    for(; i < 3; i++)
-    console.log(sIO.on("hello",function(){
-        console.log("works!");
-    }));
-    
-    setInterval(function(){
-        console.log(sIO.on("help",function(){}));
-    }, 5);
 
     
   });
