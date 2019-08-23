5fcdd93377b3455b7294e929dffcff6a937d66d9
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-22 17:52:51 -0700

    securities and sanitization

--- server.js
+++ server.js
@@ -313,22 +313,7 @@ app.use((err, req, res, next) => {
 //chat
 require("./libs/chat.js").sockets(https);
 
-var entityMap = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;',
-  "'": '&#39;',
-  '/': '&#x2F;',
-  '`': '&#x60;',
-  '=': '&#x3D;'
-};
 
-function escapeHtml (string) {
-  return String(string).replace(/[&<>"'`=\/]/g, function (s) {
-    return entityMap[s];
-  });
-}
 
 
 var usernames = {};
