1a101bbaee9259a73dfbe3e16379cb935e8213c5
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-23 15:50:09 -0700

    securities and sanitization

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ ca: fs.readFileSync('./config/ssli/anomic_io.ca-bundle'),
 key: fs.readFileSync('./config/ssli/private.key'),
 requestCert: false,
 rejectUnauthorized: false,
+maxHttpBufferSize: 128,
 },app);
 server.listen(443);
 
