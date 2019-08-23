b456948ebebc0078f07d462c0fe1c6dc27a8004d
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-22 17:34:49 -0700

    securities and sanitization

--- server.js
+++ server.js
@@ -346,7 +346,7 @@ io.sockets.on('connection', function (socket) {
 
 	// when the client emits 'sendchat', this listens and executes
 	socket.on('sendchat', function (data) {
-    message = sanitize(data.message).xss()
+    message = sanitize(data.message);
 		// we tell the client to execute 'updatechat' with 2 parameters
 		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
 	});
