f88a101f0c16c827f3f62c4ba4781bc56459ff17
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-23 13:05:05 -0700

    securities and sanitization

--- public/js/app.js
+++ public/js/app.js
@@ -48,7 +48,7 @@ var socket = io.connect('anomic.io/');
   socket.on('serverupdatechat', function (server) {
     $('#conversation').append('<div class="ui container"><div class="ui medium basic segment"></div></div>');
         $('#scrollable').animate({ scrollTop: 		$('#scrollable').prop('scrollHeight')}, 100);
-        $('#conversation').append($('<div class="ui grey label"id="message"><div id="messagedata"><p></p></div></div>').text(server));
+        $('#conversation').append($('<div class="ui large grey label"><p></p></div>').text(server));
           });
 //$('#publisher').append('<h4>' + username + '</h4>');
 
