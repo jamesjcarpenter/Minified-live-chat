4ae46aba0d72da37c608994ecaad47274f07e5fa
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-22 18:05:27 -0700

    securities and sanitization

--- public/js/app.js
+++ public/js/app.js
@@ -29,6 +29,10 @@ var socket = io.connect('anomic.io/');
 		socket.emit('adduser', prompt("Enter username."));
 	});
   // create our webrtc connection
+  socket.on('updatechat', function (username, data) {
+    $('#conversation').append('<div class="ui container"><h6>' +'<h6>' + '<div class="ui medium basic segment"><span class="ui small white text"><p>'+ '<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"><tag id="username">' +  JSON.stringify(user.name) + '</tag>' + '<samp><em>' + '</em></samp>' + '</p></span><span class="ui tiny white text"><div class="ui medium left pointing label"id="message">' + '</span><p><span class="ui medium white text"id="messagedata">' + JSON.stringify(data) + '</div></p></span></div></div>' + '</h6></div></div>');
+    $('#scrollable').animate({ scrollTop: 		$('#scrollable').prop('scrollHeight')}, 100);
+    $("#data").focus();
     
 
     
