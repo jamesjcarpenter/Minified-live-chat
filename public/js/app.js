3e301d04043d46c16614560148dabf2adad83eae
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-23 10:09:02 -0700

    securities and sanitization

--- public/js/app.js
+++ public/js/app.js
@@ -33,8 +33,8 @@ var socket = io.connect('anomic.io/');
     $('#conversation').append('<div class="ui container"><h6>' +'<h6>' + '<div class="ui medium basic segment"><span class="ui small white text"><p>'+ '<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"><tag id="username">' + '</tag>' + '<samp><em>' + '</em></samp>' + '</p></span><span class="ui tiny white text"><div class="ui medium left pointing label"id="message">' + '</span><p><span class="ui medium white text"id="messagedata">' + '</div></p></span></div></div>' + '</h6></div></div>');
     $('#scrollable').animate({ scrollTop: 		$('#scrollable').prop('scrollHeight')}, 100);
     $("#data").focus();
-    $("<div>").text(encodeURIComponent(data)).appendTo("#messagedata")
-    $("<div>").text(encodeURIComponent(username)).appendTo("#username")
+    $("<div>").text(encodeURI(data)).appendTo("#messagedata")
+    $("<div>").text(encodeURI(username)).appendTo("#username")
   //  encodeURIComponent(data);
   //  encodeURIComponent(username);
     
