d3732624bbdd35726a8242f9a7fdb51054f26c68
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-23 19:42:36 -0700

    responsive cams

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(helmet.contentSecurityPolicy({
    imgSrc: ["'self'"],
    objectSrc: ["'none'"],
    formAction: ["'self'"],
-   connectSrc: ["'self'", 'https://anomic.io:*', 'wss://anomic.io', 'https://anomic.io'],
+   connectSrc: ["'self'", 'https://anomic.io:*', 'wss://anomic.io:', 'https://anomic.io:'],
    frameSrc: ["'self'", 'https://www.webrtc-experiment.com/'],
    upgradeInsecureRequests: true,
    workerSrc: false,
