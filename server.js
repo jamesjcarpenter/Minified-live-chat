a1b4c897372b7ec3510783254340c7599a40b88a
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-23 18:47:00 -0700

    responsive cams

--- server.js
+++ server.js
@@ -51,21 +51,21 @@ app.use(function(req, res, next) {
 app.use(helmet())
 
 
-// app.use(helmet.contentSecurityPolicy({
-//  directives: {
-//    defaultSrc: ["'self'"],
-//    scriptSrc: ["'self'", "'unsafe-inline'", 'https://code.jquery.com/', 'https://maxcdn.bootstrapcdn.com/', 'https://cdnjs.cloudflare.com/', 'https://toert.github.io', 'https://www.webrtc-experiment.com/' ],
-//    styleSrc: ["'self'", "'unsafe-inline'", 'https://maxcdn.bootstrapcdn.com/', 'https://toert.github.io/', 'https://fonts.googleapis.com/'],
-//    fontSrc: ["'self'", 'https://maxcdn.bootstrapcdn.com/', 'https://fonts.googleapis.com/'],
-//    imgSrc: ["'self'"],
-//    objectSrc: ["'none'"],
-//    formAction: ["'self'"],
-//    connectSrc: ['https://anomic.io:8088/janus', 'https://anomic.io:8089/janus', 'https://anomic.io/socket.io/?EIO=3&transport=polling&t=MoxfJXe', 'wss://anomic.io/socket.io/?EIO=3&transport=websocket&sid=xklOxe_Awk3pPMfCAAAB'],
-//    frameSrc: ["'self'", 'https://www.webrtc-experiment.com/']
-// 
-//  },
-//  browserSniff: false
-// }));
+app.use(helmet.contentSecurityPolicy({
+ directives: {
+   defaultSrc: ["'https://anomic.io:*'"],
+   scriptSrc: ["'self'", "'unsafe-inline'", 'https://code.jquery.com/', 'https://maxcdn.bootstrapcdn.com/', 'https://cdnjs.cloudflare.com/', 'https://toert.github.io', 'https://www.webrtc-experiment.com/' ],
+   styleSrc: ["'self'", "'unsafe-inline'", 'https://maxcdn.bootstrapcdn.com/', 'https://toert.github.io/', 'https://fonts.googleapis.com/'],
+   fontSrc: ["'self'", 'https://maxcdn.bootstrapcdn.com/', 'https://fonts.googleapis.com/'],
+   imgSrc: ["'self'"],
+   objectSrc: ["'none'"],
+   formAction: ["'self'"],
+   connectSrc: ['https://anomic.io:*', '/*', '/janus', 'https://anomic.io/socket.io/?EIO=3&transport=polling&t=MoxfJXe', 'wss://anomic.io/socket.io/?EIO=3&transport=websocket&sid=xklOxe_Awk3pPMfCAAAB'],
+   frameSrc: ["'self'", 'https://www.webrtc-experiment.com/']
+
+ },
+ browserSniff: false
+}));
 
 const uuidv4 = require('uuid/v4')
 
