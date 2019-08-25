c600a7957eaf16ef7e78d4094a25d281ea64bbeb
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-24 19:35:38 -0700

    fixing csp guidelines

--- libs/ratelimiter.js
+++ libs/ratelimiter.js
@@ -9,7 +9,7 @@ const redisClient = redis.createClient({
 const rateLimiter = new RateLimiterRedis({
   redis: redisClient,
   keyPrefix: 'middleware',
-  points: 5, // 60 requests
+  points: 2, // 60 requests
   duration:1,
 });
 
--- server.js
+++ server.js
@@ -192,7 +192,7 @@ const redisClient = redis.createClient({
 const rateLimiter = new RateLimiterMemory(
   {
     storeClient: redisClient,
-    points: 7, // Number of points
+    points: 3, // Number of points
     duration: 1, // Per 60 seconds,
     blockDuration: 120, // Block duration in store
     inmemoryBlockOnConsumed: 301, // If userId or IP consume >300 points per minute
