d8aeef8552736585d3344c3d8311c32ad203d0ac
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-26 00:01:44 -0700

    css

--- public/js/app.js
+++ public/js/app.js
@@ -30,8 +30,7 @@ function fixedEncodeURIComponent(str) {
 var date = JSON.stringify(new Date(Date.now()).toLocaleTimeString())
   // toggle sidebar
 var socket = io.connect('anomic.io/');
-var usernames = {};
-var socket.username = username;
+
   // Add validation rules to Create/Join Room Form
   socket.on('connect', function(){
 		// call the server-side function 'adduser' and send one parameter (value of prompt)
@@ -45,7 +44,7 @@ var socket.username = username;
         return false;
       },
       onApprove : function() {
-            $('#username').append(username);
+            $('#username').append($('<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"></img><tag id="username"><h6></h6></tag>').text(username));
         window.close();
       }
     
