f8d0a6adb07e16b676334b64da93230b93e141b6
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-25 23:25:51 -0700

    css

--- public/js/app.js
+++ public/js/app.js
@@ -32,7 +32,7 @@ var date = JSON.stringify(new Date(Date.now()).toLocaleTimeString())
 var socket = io.connect('anomic.io/');
 
   // Add validation rules to Create/Join Room Form
-  socket.on('connect', function(username){
+  socket.on('connect', function('adduser', username){
 		// call the server-side function 'adduser' and send one parameter (value of prompt)
     socket.emit('adduser',
     $('.ui.modal')
