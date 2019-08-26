201c29b95fd8b71f2af735d9a49bd86262ade02b
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-26 06:26:33 -0700

    css

--- videoroom.js
+++ videoroom.js
@@ -125,7 +125,7 @@ $(document).ready(function() {
 											console.log("new instant room = " + "https://anomic.io/" + room);
 											window.location = "https://anomic.io/" + "room?name=" + room;
 											var newRoom = { "request": "create", "room": room, "ptype": "publisher", "display": socket.username };
-											document.getElementById("#start").onclick = sfutest.send({"message": newRoom});;
+											sfutest.send({"message": newRoom});;
 											janus.destroy();
 										});
 								},
