bba3e2513bd5c1bc3dfe6bee3af81546441e7a3a
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-27 13:12:39 -0700

    css

--- routes/index.js
+++ routes/index.js
@@ -96,7 +96,7 @@ router.post('/newroom', function(req, res, next) {
     // save the user
     newRoom.save(function(err) {
       if (err) throw err;
-      req.body.name1 = room.name;
+      req.body.name1 = req.room.name;
       console.log('Room created!');
       res.redirect('/room?name=' + '' + req.body.name1);
       res.render('index.ejs', { room: req.room.name, chat: req.session.chat });
