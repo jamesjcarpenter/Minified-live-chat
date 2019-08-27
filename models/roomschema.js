1a4ad831939d626a7ea8e9fa6fb3da927d9bbb34
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-27 08:02:47 -0700

    css

--- models/roomschema.js
+++ models/roomschema.js
@@ -11,6 +11,13 @@ const RoomSchema = new mongoose.Schema({
 });
 var Room = module.exports = mongoose.model('Room', RoomSchema);
 
+var room2 = new Room({ name1: 'room2', name2: 'room2', members: [], lastActive: '', createdOn: '' });
+
+room2.save(function (err, room) {
+     if (err) return console.error(err);
+     console.log(room.name + " saved to room collection.");
+   });
+
 function saveRoom(data) {
     var newRoom = new Room(data);
     newRoom.save(function (err) {
