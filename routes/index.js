95dc6e535f6c1aa7542e2c3fd787a440b6e178c4
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-27 12:04:53 -0700

    css

--- routes/index.js
+++ routes/index.js
@@ -76,6 +76,7 @@ router.post('/newroom', function(req, res, next) {
     
     //User is the model created in app.js of this project
     var newRoom = new Room({
+      _id:  mongoose.Schema.Types.ObjectId,
       name1: req.body.name1,
       name2: req.body.name1,
       members: [],
