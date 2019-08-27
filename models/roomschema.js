5d247af9dfbe642422581e27d867d2499916e910
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-27 08:30:07 -0700

    css

--- models/roomschema.js
+++ models/roomschema.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const express = require('express')
 const session = require('express-session')
 
-const RoomSchema = new mongoose.Schema({
+const roomSchema = new mongoose.Schema({
   name1: { type: String, default: "", },
    name2: { type: String, default: "",},
    members: [],
    lastActive: { type: Date, default: Date.now },
    createdOn: { type: Date, default: Date.now }
 });
-var Room = module.exports = mongoose.model('Room', RoomSchema);
+var Room = module.exports = mongoose.model('Room', roomSchema);
 
 var room2 = new Room({ name1: 'real9k', name2: 'real9k', members: [], lastActive: '', createdOn: '8/27/2019' });
 
