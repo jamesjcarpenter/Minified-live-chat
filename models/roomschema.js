ab41386e56b4d07853ea585886e4839135fc1a83
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-27 08:45:02 -0700

    css

--- models/roomschema.js
+++ models/roomschema.js
@@ -3,6 +3,7 @@ const express = require('express')
 const session = require('express-session')
 
 const RoomSchema = new mongoose.Schema({
+  _id :  { type: Number },
   name1: { type: String, default: "", },
    name2: { type: String, default: "",},
    members: [],
