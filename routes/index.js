12230d0b3c40272ff68d5d786c790072ce88f9ea
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-27 09:05:35 -0700

    css

--- routes/index.js
+++ routes/index.js
@@ -76,7 +76,6 @@ router.post('/newroom', function(req, res, next) {
     
     //User is the model created in app.js of this project
     var newRoom = new Room({
-      id: _id,
       roomId: id,
       name1: req.body.name1,
       name2: req.body.name1,
