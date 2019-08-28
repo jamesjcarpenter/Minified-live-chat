c6680c776e14766f702f45a3498431327bf14c73
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-27 21:12:47 -0700

    css

--- public/js/scriptForChat.js
+++ public/js/scriptForChat.js
@@ -1,6 +1,6 @@
 $ (function(){
 
-  var socket = io('/chat');
+  var socket = io.connect('anomic.io/443');
 
   var username = $('#user').val();
   var noChat = 0; //setting 0 if all chats histroy is not loaded. 1 if all chats loaded.
