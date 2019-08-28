51f9b4a81279c6f0ef7c2f94ecfa256279c29d47
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-28 02:41:25 -0700

    css

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const Room = require("./models/roomschema");
 const Image = require("./models/profileimg");
 const mongo = require('mongodb');
 const mongoose = require('mongoose');
-const db = require('config/keys').MongoURI;
+const db = require('/config/keys').MongoURI;
 mongoose.connect(db, { useNewUrlParser: true })
 .then(() => console.log('MongoDB connected..'))
 .catch(err => console.log(err));
