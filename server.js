63b2cb8e4da7b5b8980a83a49ff141ff227a2f4b
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-27 19:29:05 -0700

    css

--- server.js
+++ server.js
@@ -343,14 +343,20 @@ io.sockets.on('connection', function (socket) {
         .to(socket.room)
         .broadcast.emit("typing", socket.username + " : is typing...");
     });
-    eventEmitter.emit("get-room-data", room);
-    //setting room and join.
-    setRoom = function(roomId) {
-      socket.room = roomId;
-      console.log("roomId : " + socket.room);
-      socket.join(socket.room);
-      ioChat.to(userSocket[socket.username]).emit("set-room", socket.room);
-    }; //end of set-room event.
+    socket.on("set-room", function(room) {
+      //leaving room.
+      socket.leave(socket.room);
+      //getting room data.
+      eventEmitter.emit("get-room-data", room);
+      //setting room and join.
+      setRoom = function(roomId) {
+        socket.room = roomId;
+        console.log("roomId : " + socket.room);
+        socket.join(socket.room);
+        ioChat.to(userSocket[socket.username]).emit("set-room", socket.room);
+      };
+    }); //end of set-room event.
+
 
 		// echo to client they've connected
 		socket.emit('serverupdatechat', 'Connected to room1');
