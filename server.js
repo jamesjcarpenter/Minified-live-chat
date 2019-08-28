da68ee367d9c94b78f42be75832e13c5829cac32
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-27 17:04:18 -0700

    css

--- server.js
+++ server.js
@@ -357,17 +357,17 @@ io.sockets.on('connection', function (socket) {
 		// store the username in the socket session for this client
 		socket.username = username;
 		// store the room name in the socket session for this client
-		socket.room = newRoom;
+		socket.room = newRoom.name1;
     
 		// add the client's username to the global list
 		usernames[username] = username;
 		// send client to room 1
-		socket.join(room.newRoom);
+		socket.join(newRoom.name1);
 		// echo to client they've connected
-		socket.emit('serverupdatechat', 'Connected to' + room.newRoom);
+		socket.emit('serverupdatechat', 'Connected to' + newRoom.name1);
 		// echo to room 1 that a person has connected to their room
 	//	socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-		socket.emit('updaterooms', rooms, room.newRoom);
+		socket.emit('updaterooms', rooms, newRoom.name1);
 	});
 
 	// when the client emits 'sendchat', this listens and executes
