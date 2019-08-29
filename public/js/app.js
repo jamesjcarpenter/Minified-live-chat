b6ed8b662edab06e76565c337102a2a86cbc6937
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-29 03:47:24 -0700

    pcg

--- public/js/app.js
+++ public/js/app.js
@@ -71,23 +71,23 @@ $('#datasend').submit(function(){
 
 
   // on load of page
-socket.on('connect', function(data) {
-    // when the client clicks SEND
-    // $('#datasend').click( function() {
-    //   var message = $('#data').val();
-    //   $('#data').val('');
-    //   // tell server to execute 'sendchat' and send along one parameter
-    //   socket.emit('sendchat', message);
-    // });
-
-    // when the client hits ENTER on their keyboard
-    $('#data').keypress(function(e) {
-      if(e.which == 13) {
-        $(this).blur();
-        $('#datasend').focus().click();
-      }
-    });
-  });
+// socket.on('connect', function(data) {
+//     // when the client clicks SEND
+//     $('#datasend').click( function() {
+//       var message = $('#data').val();
+//       $('#data').val('');
+//       // tell server to execute 'sendchat' and send along one parameter
+//       socket.emit('sendchat', message);
+//     });
+// 
+//     // when the client hits ENTER on their keyboard
+//     $('#data').keypress(function(e) {
+//       if(e.which == 13) {
+//         $(this).blur();
+//         $('#datasend').focus().click();
+//       }
+//     });
+//   });
   socket.on('updateroomusers', function(roomusers, username) {
   $("#roomusers").empty();
   $.each(roomusers, function (key, value) {
