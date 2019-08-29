003c5ffcc1981a2beb5446fbc733d3163ffe01a3
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-29 03:32:12 -0700

    pcg

--- models/chat.js
+++ models/chat.js
@@ -3,10 +3,10 @@ const express = require('express')
 const session = require('express-session')
 
 const ChatSchema = new mongoose.Schema({
-  msgFrom: { type: String, default: "", required: true },
-  msgTo: { type: String, default: "", required: true },
-  msg: { type: String, default: "", required: true },
-  room: { type: String, default: "", required: true },
+  msgFrom: { type: String, default: '', required: true },
+  msgTo: { type: String, default: '', required: true },
+  msg: { type: String, default: '', required: true },
+  room: { type: String, default: '', required: true },
   createdOn: { type: Date, default: Date.now }
 });
 
