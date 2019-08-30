e5dacef58e34bc36bd5cb7b78cc9868f24166380
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-08-29 20:19:39 -0700

    pcg

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
 $('#start').click();
 $('#start').hide();
 $('#bitrateset').hide();
-var roomId;//variable for setting room.
+
 var url = window.location.href;
 
 console.log(url);
@@ -35,23 +35,32 @@ function fixedEncodeURIComponent(str) {
   });
 }
 
+var date = JSON.stringify(new Date(Date.now()).toLocaleTimeString())
   // toggle sidebar
+var socket = io.connect('anomic.io/');
+
   // Add validation rules to Create/Join Room Form
   socket.on('connect', function(){
 		// call the server-side function 'adduser' and send one parameter (value of prompt)
 		socket.emit('adduser', prompt("Enter username."));
+	});
   // create our webrtc connection
   socket.on('updatechat', function (username, data) {
-    $('#conversation').append('<div class="ui container"><h6>' +'<h6>' + '<div class="ui medium basic segment"><span class="ui small white text"><p>'+ '<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"><tag id="username">' + encodeURIComponent(JSON.stringify(username))) + '</tag>' + '<samp><em>' + '</em></samp>' + '</p></span><span class="ui tiny white text"><div class="ui medium left pointing label"id="message">' + '</span><p><span class="ui medium white text"id="messagedata">' + encodeURIComponent(JSON.stringify(data))) + '</div></p></span></div></div>' + '</h6></div></div>');
+    $('#conversation').append('<div class="ui container"><div class="ui medium basic segment"></div></div>');
     $('#scrollable').animate({ scrollTop: 		$('#scrollable').prop('scrollHeight')}, 100);
     $("#data").focus();
-    $('#scrollable').animate({ scrollTop: 		$('#scrollable').prop('scrollHeight')}, 100);
-    $("#data").focus();
-
-
+    $('#usercam').empty().append($('<span class="ui text small "></span>').text(username));
+    $('#conversation').append($('<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"></img><tag id="username"name="avatar"><h6></h6></tag>').text(username));
+    $('#conversation').append($('<span class="ui small text" id="date"name="date"></span>').text(JSON.parse(date)));
+    $('#conversation').append($('<div class="ui left pointing label"id="message"name="data"><div id="messagedata"><p></p></div></div>').text(data));
+    
   });
   // listener, whenever the server emits 'updaterooms', this updates the room the client is in
-
+  socket.on('serverupdatechat', function (server) {
+    $('#conversation').append('<div class="ui container"><div class="ui medium basic segment"></div></div>');
+        $('#scrollable').animate({ scrollTop: 		$('#scrollable').prop('scrollHeight')}, 100);
+        $('#conversation').append($('<div class="ui large grey label"id="servermessage"><p></p></div>').text(server));
+          });
 //$('#publisher').append('<h4>' + username + '</h4>');
 
   // on load of page
@@ -72,13 +81,13 @@ socket.on('connect', function(data) {
       }
     });
   });
-  socket.on('updateroomusers', function(roomusers) {
+  socket.on('updateroomusers', function(roomusers, username) {
   $("#roomusers").empty();
   $.each(roomusers, function (key, value) {
   $('#roomusers').append('+value+');
   });
   });
-
+  
   socket.on('updaterooms', function(rooms, current_room) {
       $('#rooms').empty();
       $.each(rooms, function(key, value) {
@@ -90,98 +99,6 @@ socket.on('connect', function(data) {
         }
       });
     });
-
-    function switchRoom(room){
-      socket.broadcast.emit('switchRoom', room);
-    }
-
-  socket.on('onlineStack',function(stack){
-     $('#list').empty();
-     $('#list').append($('<li>').append($('<button id="ubtn" class="btn btn-danger btn-block btn-lg"></button>').text("Group").css({"font-size":"18px"})));
-     var totalOnline = 0;
-     for (var user in stack){
-       //setting txt1. shows users button.
-       if(user == username){
-         var txt1 = $('<button class="boxF disabled"> </button>').text(user).css({"font-size":"18px"});
-       }
-       else{
-         var txt1 = $('<button id="ubtn" class="btn btn-success  btn-md">').text(user).css({"font-size":"18px"});
-       }
-       //setting txt2. shows online status.
-       if(stack[user] == "Online"){
-         var txt2 = $('<span class="badge"></span>').text("*"+stack[user]).css({"float":"right","color":"#009933","font-size":"18px"});
-         totalOnline++;
-  
-       }
-       else{
-         var txt2 = $('<span class="badge"></span>').text(stack[user]).css({"float":"right","color":"#a6a6a6","font-size":"18px"});
-       }
-       //listing all users.
-       $('#list').append($('<li>').append(txt1,txt2));
-       $('#totalOnline').text(totalOnline);
-     }//end of for.
-     $('#scrl1').scrollTop($('#scrl1').prop("scrollHeight"));
-   }); //end of receiving onlineStack event.
-
-  //create an array to hold all the usernames of the poeple in a specific room
- var roomusers = [];
-  // get all the clients in ‘room1′
- var clients = io.sockets.adapter.rooms['r9k'];
- clients.length;
-  // loop through clients in ‘room1′ and add their usernames to the roomusers array
- for(var i = 0; i < clients.length; i++) {
- roomusers[roomusers.length] = clients[i].username;
- }
-  // broadcast to everyone in room 1 the usernames of the clients connected.
- io.sockets.to('r9k').emit('updateroomusers', roomusers);
- // 
- //  index.html code
- // 
- // 
- //  Post Local Message
- // 
- // 
- //  Display Chat Interface
- // const showChatRoom = (room) => {
- //   formEl.hide();
- //   const html = chatTemplate({ room });
- //   chatEl.html(html);
- //   const postForm = $('form');
- //   postForm.form({
- //     message: 'empty',
- //   });
- //   $('#post-btn').on('click', () => {
- //     const message = $('#post-message').val();
- //     postMessage(message);
- //   });
- //   $('#post-message').on('keyup', (event) => {
- //     if (event.keyCode === 13) {
- //       const message = $('#post-message').val();
- //       postMessage(message);
- //     }
- //   });
- // };
- // 
- //  Register new Chat Room
- // 
- // 
- //  Join existing Chat Room
- // const joinRoom = (roomName) => {
- //    eslint-disable-next-line no-console
- //   console.log(`Joining Room: ${roomName}`);
- //   webrtc.joinRoom(roomName);
- //   showChatRoom(roomName);
- //   postMessage(`${username} joined chatroom`);
- // };
- // 
- //  Receive message from remote user
- //   webrtc.connection.on('message', (data) => {
- //   if (data.type === 'chat') {
- //     const message = data.payload;
- //     messages.push(message);
- //     updateChatMessages();
- //  }
- // });
 
     // function switchRoom(room){
     //   socket.broadcast.emit('switchRoom', room);
