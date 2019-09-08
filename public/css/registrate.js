f3a4547ac8decfe1ccc792328c0013758bb80967
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-08 08:53:59 -0700

    .

---
+++ public/css/registrate.js
@@ -0,0 +1,45 @@
+$(document)
+  .ready(function() {
+    $('.ui.form')
+      .form({
+        fields: {
+          email: {
+            identifier  : 'email',
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'Please enter your e-mail'
+              },
+              {
+                type   : 'email',
+                prompt : 'Please enter a valid e-mail'
+              }
+            ]
+          },
+          name: {
+            identifier  : 'name',
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'Please enter a valid username'
+              }
+            ]
+          },
+          password: {
+            identifier  : 'password',
+            rules: [
+              {
+                type   : 'empty',
+                prompt : 'Please enter your password'
+              },
+              {
+                type   : 'length[6]',
+                prompt : 'Your password must be at least 6 characters'
+              }
+            ]
+          }
+        }
+      })
+    ;
+  })
+;
