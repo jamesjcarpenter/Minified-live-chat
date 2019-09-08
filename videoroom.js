56e2affd7c6aa55fa08a0b86dcfca645d61b9a83
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-08 07:00:43 -0700

    .

--- videoroom.js
+++ videoroom.js
@@ -96,8 +96,8 @@ $(document).ready(function() {
 							//		$('#registernow').removeClass('hide').show();
 							//		$('#register').click(registerUsername);
 					//				$('#username').focus();
-									var register = { "request": "join", "room": myroom, "ptype": "publisher", "display": socket.username };
-									sfutest.send({"message": register});
+									// var register = { "request": "join", "room": myroom, "ptype": "publisher", "display": socket.username };
+									// sfutest.send({"message": register});
 									$('#start').removeAttr('disabled').html("Stop")
 										.click(function() {
 											$(this).attr('disabled', true);
