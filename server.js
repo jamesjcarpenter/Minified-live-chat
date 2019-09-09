0d7f114ac048bff5cb9ab6cd8b6698e646a8f9d9
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-09 16:15:09 -0700

    .

--- server.js
+++ server.js
@@ -360,7 +360,11 @@ io.sockets.on('connection', function (socket) {
 		// store the username in the socket session for this client
 		socket.username = username;
 		// store the room name in the socket session for this client
-		socket.room = roomName;
+    function myFunction(req, res, next {
+      req.query.name = socket.room;
+      var room = req.query.name;
+    });
+		socket.room = req.query.name;
 		// add the client's username to the global list
 		usernames[username] = username;
 		// send client to room 1
