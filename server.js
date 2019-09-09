08a466f5241792a8a1ac224ae762c1444d22595d
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-09 16:09:59 -0700

    .

--- server.js
+++ server.js
@@ -292,8 +292,6 @@ app.post('/api/images', parser.single("image"), (req, res) => {
 
 
 app.use(function(req, res, next) {
-  console.dir(req.query.name)
-  var room = req.query.name;
   res.locals.user = req.user || null;
     if(req.user == null){
       username = 'guest';
