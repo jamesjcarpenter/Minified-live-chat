9d1e0dc7d5f97c70a5b2f57dc7b090de9f1c12c9
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-09 15:06:38 -0700

    .

--- server.js
+++ server.js
@@ -209,6 +209,8 @@ app.use(express.static(__dirname + '/'));
 app.use(function(req, res, next) {
     res.locals.user = req.user; // This is the important line
     exports.token = req.user;
+    console.dir(req.query.name)
+    var myRoom = req.query.name;
     next();
 });
 var routes = require('./routes/index.js');
@@ -344,11 +346,7 @@ var rooms = ['1','2','3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', 
 // usernames which are currently connected to the chat
 var usernames = {};
 // rooms which are currently available in chat
-app.use(function(req, res, next) {
-console.dir(req.query.name)
-var myRoom = req.query.name;
-next();
-});
+
 
 
 io.sockets.on('connection', function (socket) {
