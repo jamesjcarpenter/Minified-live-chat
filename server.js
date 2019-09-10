ac26a6784e8ee0510134528ae9257189f4eed59e
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-09 18:08:10 -0700

    .

--- server.js
+++ server.js
@@ -354,17 +354,22 @@ var usernames = {};
 // rooms which are currently available in chat
 
 io.sockets.on('connection', function (socket) {
-
+  
+  socket.on('joinroom', function(room) {
+     this.join(room);
+     if (typeof rooms[room] ==== "undefined") rooms[room] = {};
+     rooms[room].count = rooms[room].total ? rooms[room].total+1 : 1; 
+     io.to(room).emit("new user", rooms[room].count)
+ });
 	// when the client emits 'adduser', this listens and executes
 	socket.on('adduser', function(username){
 		// store the username in the socket session for this client
 		socket.username = username;
 		// store the room name in the socket session for this client
-		socket.room = 'room1';
+
 		// add the client's username to the global list
 		usernames[username] = username;
 		// send client to room 1
-		socket.join('room1');
 		// echo to client they've connected
 		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
 		// echo to room 1 that a person has connected to their room
