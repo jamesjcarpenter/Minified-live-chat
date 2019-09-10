8ba7be0026dd766733a50845daa5317c4e319efb
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-09 19:48:01 -0700

    .

--- server.js
+++ server.js
@@ -364,7 +364,7 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	// when the client emits 'sendchat', this listens and executes
-	socket.on('updatechat', function (data) {
+	socket.on('sendchat', function (data) {
 		// we tell the client to execute 'updatechat' with 2 parameters
 		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
 	});
