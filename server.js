1f7f7d24336cf8a7f8db781dac3b85d033b45149
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-10 00:24:15 -0700

    .

--- server.js
+++ server.js
@@ -337,7 +337,7 @@ app.use((err, req, res, next) => {
   });
 });
 //chat
-require("./libs/chat.js").sockets(server);
+require("./libs/chat.js").sockets(https);
 
 
 var rooms = ['1','2','3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
