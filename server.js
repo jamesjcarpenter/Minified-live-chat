16d1c113a2647fc8e8145c2f12abe4c4be16081b
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-10 18:24:44 -0700

    .

--- server.js
+++ server.js
@@ -353,11 +353,11 @@ app.use((err, req, res, next) => {
 var rooms = ['1','2','3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
 // usernames which are currently connected to the chat
 var usernames = {};
+var url = window.location;
 // rooms which are currently available in chat
 var window = require('window-or-global');
 io.on('connection', function (socket) {
     //leaving room.
-    var url = window.location;
     socket.room = {};
     var roomId = url.substr(url.lastIndexOf("=")+1);
     socket.room = roomId;
