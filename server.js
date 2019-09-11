6439cb9b83e26a14d87937c383b2ddab1f43b7bf
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-10 19:12:47 -0700

    .

--- server.js
+++ server.js
@@ -367,7 +367,8 @@ io.on('connection', function (socket) {
     
     socket.join(socket.room);
     //event to get chat history on button click or as room is set.
-
+    console.log(socket.room);
+    console.log(roomName);
 
 	// when the client emits 'adduser', this listens and executes
 	socket.on('adduser', function(username){
