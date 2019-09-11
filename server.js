ccd5fe0072ad506b0b28df5d8e03d9de9fc551d2
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-11 00:20:09 -0700

    .

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ server.listen(443);
 var router = express.Router();
 
 //make sure you keep this order
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(port);
 
 //... 
 //..
@@ -387,41 +387,43 @@ io.on('connection', function (socket) {
       setRoom = function(roomId) {
         socket.room = roomId;
         console.log("roomId : " + socket.room);
-        // socket.join(socket.room);
+        socket.join(socket.room);
         ioChat.to(usernames[socket.username]).emit("set-room", socket.room);
       };
     });
+
+    socket.join(socket.room);
   
 	  // when the client emits 'adduser', this listens and executes
 		// send client to room 1
 		// echo to client they've connected
 		socket.emit('updatechat', 'SERVER', 'you have connected to room1');
 		// echo to room 1 that a person has connected to their room
-		socket.to(socket.room).emit('updatechat', 'SERVER', username + ' has connected to this room');
+		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', username + ' has connected to this room');
 		socket.emit('updaterooms', rooms, socket.room);
 	});
 
 	// when the client emits 'sendchat', this listens and executes
   socket.on('sendchat', function (data) {
   		// we tell the client to execute 'updatechat' with 2 parameters
-  		io.in(socket.room).emit('updatechat', socket.username, data);
+  		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
   	});
 
 
 
-	socket.on('switchRoom', function(newroom){
-		// leave the current room (stored in session)
-		socket.leave(socket.room);
-		// join new room, received as function parameter
-		socket.join(newroom);
-		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
-		// sent message to OLD room
-		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username +' has left this room');
-		// update socket session room title
-		socket.room = newroom;
-		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-		socket.emit('updaterooms', rooms, newroom);
-	});
+	// socket.on('switchRoom', function(newroom){
+	// 	// leave the current room (stored in session)
+	// 	socket.leave(socket.room);
+	// 	// join new room, received as function parameter
+	// 	socket.join(newroom);
+	// 	socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+	// 	// sent message to OLD room
+	// 	socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username +' has left this room');
+	// 	// update socket session room title
+	// 	socket.room = newroom;
+	// 	socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+	// 	socket.emit('updaterooms', rooms, newroom);
+	// });
 
 	// when the user disconnects.. perform this
 	socket.on('disconnect', function(){
