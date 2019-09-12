baf316023b7d2443a801ff83f6df9635b43fc4bd
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-12 00:42:32 -0700

    .

--- public/js/streaming.js
+++ public/js/streaming.js
@@ -44,9 +44,9 @@
 //
 var server = null;
 if(window.location.protocol === 'http:')
-	server = "http://" + window.location.hostname + ":8088/janus";
+	server = "wss://" + window.location.hostname + ":8088/janus";
 else
-	server = "https://" + window.location.hostname + ":8089/janus";
+	server = "wss://" + window.location.hostname + ":8089/janus";
 
 var janus = null;
 var streaming = null;
--- videoroom.js
+++ videoroom.js
@@ -44,9 +44,9 @@
 //
 var server = null;
 if(window.location.protocol === 'http:')
-	server = "ws://" + window.location.hostname + ":8088/janus";
+	server = "http://" + window.location.hostname + ":8088/janus";
 else
-	server = "wss://" + window.location.hostname + ":8089/janus";
+	server = "https://" + window.location.hostname + ":8089/janus";
 
 var janus = null;
 var sfutest = null;
