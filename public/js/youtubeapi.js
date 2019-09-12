1025f06a8e6ca44494d6f82a30c46d0a6460c0f4
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-12 03:33:01 -0700

    .

--- public/js/youtubeapi.js
+++ public/js/youtubeapi.js
@@ -1,27 +1,6 @@
 window.addEventListener('load', () => {
 
 init();
-$("form").on("submit", function(e) {
-  e.preventDefault();
-  var request = gapi.client.youtube.search.list({
-    part: "snippet",
-    type: "video",
-    q: encodeURIComponent($("#search").val()).replace(/%/g, "+"),
-    maxResults: 5,
-    order: "viewCount",
-  });
-  //executre
-  request.execute(function(response) {
-    var results = response.result;
-    $("#results").html("");
-    $.each(results.item, function(index, item) {
-      $.get("layout.handlebars", function(data) {
-        $("#results").append(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]);
-      });
-    });
-    resetVideoHeight();
-  });
-});
 
 $(window).on("resize", resetVideoHeight);
 
@@ -33,6 +12,27 @@ function init() {
   gapi.client.setApiKey("AIzaSyCuKhQw-AouTjuiEIKquFiJuiWgpffr-LM");
   gapi.client.load("youtube", "v3", function() {
     //yt api is ready
+    $("form").on("submit", function(e) {
+      e.preventDefault();
+      var request = gapi.client.youtube.search.list({
+        part: "snippet",
+        type: "video",
+        q: encodeURIComponent($("#search").val()).replace(/%/g, "+"),
+        maxResults: 5,
+        order: "viewCount",
+      });
+      //executre
+      request.execute(function(response) {
+        var results = response.result;
+        $("#results").html("");
+        $.each(results.item, function(index, item) {
+          $.get("layout.handlebars", function(data) {
+            $("#results").append(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]);
+          });
+        });
+        resetVideoHeight();
+      });
+    });
   });
 }
 });
