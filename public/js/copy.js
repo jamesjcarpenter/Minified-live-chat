0000afbc4bd976d42c0e6b3f236469ce881cd858
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-11 22:32:32 -0700

    .

--- public/js/copy.js
+++ public/js/copy.js
@@ -1,7 +1,7 @@
 window.onload = function(){ 
   
 var copyText = document.getElementById("#copyinput");
-document.getElementById(cpybtn).onclick = function() {
+document.getElementById("#cpybtn").onclick = function() {
   // Select the content
     copyText.select();
     copyText.setSelectionRange(0, 99999);
