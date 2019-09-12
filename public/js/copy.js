c09d9b11fe021e3f9f7ac5be1516579e7706778a
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-11 22:26:23 -0700

    .

--- public/js/copy.js
+++ public/js/copy.js
@@ -1,7 +1,6 @@
 window.onload = function(){ 
   
-var copybutton = document.getElementById('#cpybtn');
-let copybutton = document.querySelector("#cpybtn").onclick = function() {
+document.getElementById('#cpybtn') = document.querySelector("#cpybtn").onclick = function() {
   // Select the content
   document.querySelector("#copyinput").select();
   // Copy to the clipboard
