9cc8dfcd23cf98e84774b12894074c7f2bff4130
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-12 02:15:53 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,34 @@
 window.addEventListener('load', () => {
   
   
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    var request - gapi.client.youtube.search.list({
+      part: "snippet",
+      type: "video",
+      q: encodeURIComponent($("#search").val()).replace(/%/g, "+"),
+      maxResults: 5,
+      order: "viewCount",
+    });
+    //executre
+    request.execute(function(response) {
+      var results = response.result;
+      $.each(results.item, function(index, item) {
+        $.get("tpl/item.html", function(data) {
+          $("#results").append(data);
+        });
+          // $("#results").append(item.id.videoId+" "+item.snippet.title+"<br>");
+      });
+    });
+  });
+});
   // var socket = io.connect('anomic.io/443');
-  
+  function init() {
+    gapi.client.setApiKey("AIzaSyCuKhQw-AouTjuiEIKquFiJuiWgpffr-LM");
+    gapi.client.load("youtube", "v3", function() {
+      //yt api is ready
+    });
+  }
   // Chat platform
   // Local Video
 $('#start').click();
