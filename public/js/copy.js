8b2a17506218ad611eb216d3feab712f949d946e
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-11 22:33:21 -0700

    .

--- public/js/copy.js
+++ public/js/copy.js
@@ -1,10 +1,7 @@
-window.onload = function(){ 
-  
-var copyText = document.getElementById("#copyinput");
+var copyText = window.location.href;
 document.getElementById("#cpybtn").onclick = function() {
   // Select the content
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
   };
-};
