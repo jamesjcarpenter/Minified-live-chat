cbffcad02ee3f461bb5efd486dec527721c34e40
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-12 14:01:31 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -23,6 +23,9 @@ $('#youtubeopen').click( function() {
 });
 
 
+
+
+
     document.getElementById('themecss').href = 'css/indextheme2.css';
     $('.ui.button').addClass('inverted');
     $('.icon').addClass('inverted');
@@ -44,6 +47,140 @@ function fixedEncodeURIComponent(str) {
   return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
     return '%' + c.charCodeAt(0).toString(16);
   });
+}
+
+var tag = document.createElement('script');
+tag.src = "http://www.youtube.com/player_api";
+
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var ytId;
+var done = false;
+var startTime = 0;
+var timer1;
+var timer2;
+var firstFlag;
+var judge_term = 1;
+
+
+
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('player1', {
+        height: '315',
+        width: '560',
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+        
+function onPlayerStateChange(evt) {
+    if(evt.data == YT.PlayerState.PLAYING) {
+        if(firstFlag == 0) {
+            pause();
+            firstFlag = 1;
+            player.seekTo(0-getCurrentPosition(), true);
+            console.log("ready to start video.");
+        }
+    }
+}
+
+/*
+function setCurentQuality() {
+    var quality = player.getPlaybackQuality();
+    document.getElementById("quality").innerHTML = quality;
+}
+*/
+
+function checkBuff() {
+    var globalBuff = Math.round(player.getVideoLoadedFraction() * 100);
+    var localBuff = globalBuff - Math.round(getCurrentPosition() / player.getDuration() * 100);
+    document.getElementById("global-buff-size").innerHTML = globalBuff;
+    document.getElementById("local-buff-size").innerHTML = localBuff;
+}
+
+//3秒ずれると動画を停止(停止ボタン)
+function checkDelay() {
+    currentTime = getCurrentPosition();
+    var gap = Math.abs(startTime - currentTime);
+    if(gap >= judge_term) {
+        pauseButtonListener();
+        console.log("find gap = " + gap);
+        clearInterval(timer1);
+        startTime = currentTime;
+        pauseButtonListener();
+    }
+}
+
+function countTimeFromStart() {
+    startTime++;
+    console.log(startTime + " seconds from start.");
+    //setCurentQuality();
+    checkDelay();
+}
+
+//動画を再生してからの相対時間を計測
+function setTimerFromStart() {
+    timer1 = setInterval("countTimeFromStart()", 1000);
+}
+
+function play() {
+    player.playVideo();
+    setTimerFromStart();
+}
+
+function pause() {
+    player.pauseVideo();
+}
+
+function stopVideo() {
+    //stopVideo()は動画の読み込みも止める
+    player.stopVideo();
+}
+
+function setYouTubeId(url) {
+    ytId = url.split("watch?v=")[1];
+}
+
+function loadVideoById() {
+    firstFlag = 0;
+    startTime = 0;
+    player.loadVideoById(ytId);
+    setHistory("set: " + ytId);
+}
+
+function initialize() {
+    var url = document.getElementById("murl").value;
+    document.getElementById("murl").value = "";
+    setYouTubeId(url);
+    sendRequest("0", ytId, "0", "set");
+    loadVideoById();
+}
+
+function getCurrentPosition() {
+    var position = player.getCurrentTime();
+    return position;
+}
+
+function pauseWithSeek(host_position) {
+    player.seekTo(host_position, true);
+}
+
+function playButtonListener() {
+    sendRequest("1", "ytId", getCurrentPosition(), "start");
+    play();
+    setHistory("play: " + getCurrentPosition());
+}
+
+function pauseButtonListener() {
+    pause();
+    sendRequest("2", "ytId", getCurrentPosition(), "pause");
+    setHistory("pause: " + getCurrentPosition());
+}
+
+
+function syncButtonListener() {
+    sendRequest("3", "ytId", getCurrentPosition(), "sync");
 }
 
 
--- public/js/yt.js
+++ public/js/yt.js
@@ -1,142 +1,100 @@
-var tag = document.createElement('script');
-tag.id = 'iframe-demo';
-tag.src = 'https://www.youtube.com/iframe_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var conn;
+var destId;
+var peer = new Peer({key: '3hqzt80o2crz4cxr'});
+var request = ["start", "restart", "pause", "chat"];
 
-var player;
+function showOnStatus() {
+    document.getElementById("dst-id").diabled = "disabled";
+    document.getElementById("status").innerHTML = "connect";
+}
 
-var playerStatus = -1;
+function showOffStatus() {
+    document.getElementById("status").innerHTML = "no connection";
+}
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        playerVars: {
-            autoplay: 0,
-            rel: 0,
-            controls: 1
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
+function showWhileStatus() {
+    document.getElementById("dst-id").diabled = "disabled";
+    document.getElementById("status").innerHTML = "connecting...";
+}
+
+function setHistory(msg) {
+    console.log(msg);
+}
+
+function setChatHistory(msg) {
+    var msg_history_obj = document.getElementById("msg-history");
+    msg_history_obj.value = msg + "\n" + msg_history_obj.value;
+}
+
+function sendContent(data) {
+    conn.send(JSON.stringify(data));
+}
+
+//YouTube操作リクエストの送信
+function sendRequest(request_num, ytId, time, message) {
+    var data = {
+        "kind": request[request_num],
+        "id": ytId,
+        "time": time,
+        "msg": message
+    };
+    
+    sendContent(data);
+}
+
+//チャット送信
+function msgSend() {
+    var msg_obj = document.getElementById("msg");
+    var message = msg_obj.value;
+    msg_obj.value = "";
+    var data = {
+        "kind": "chat",
+        "id": "-1",
+        "time": "-1",
+        "msg": message
+    };
+    sendContent(data);
+    var msg_history_obj = document.getElementById("msg-history");
+    msg_history_obj.value = "You : " + message + "\n" + msg_history_obj.value;
+    
+}
+
+//受信
+function receivedContent(json_data) {
+    var rcv_data = JSON.parse(json_data);
+    requestHandler(rcv_data);
+}
+
+//接続ボタンリスナー
+function ConnectButtonListener() {
+    document.getElementById("cnt-btn").disabled = "disabled";
+    destId = document.getElementById("dst-id").value;
+    showWhileStatus();
+    conn = peer.connect(destId);
+    conn.on('open', function() {
+        conn.on('data', function(data) {
+            receivedContent(data);
+            //console.log("received: " + data);
+        });
     });
-    // document.getElementById('player').src = document.getElementById('player').src + '&controls=0'
-    console.log(document.getElementById('player').src)
+    showOnStatus();
 }
 
-function onPlayerReady(event) {
-    //document.getElementById('player').style.borderColor = '#FF6D00';
-    document.getElementById('player').style.borderColor = '#00000000';
-}
 
-function changeBorderColor(playerStatus) {
-    var color;
-    if (playerStatus == -1) {
-        color = "#37474F"; // unstarted = gray
-    } else if (playerStatus == 0) {
-        color = "#FFFF00"; // ended = yellow
-    } else if (playerStatus == 1) {
-        color = "#33691E"; // playing = green
-    } else if (playerStatus == 2) {
-        color = "#DD2C00"; // paused = red
-    } else if (playerStatus == 3) {
-        color = "#AA00FF"; // buffering = purple
-    } else if (playerStatus == 5) {
-        color = "#FF6DOO"; // video cued = orange
-    }
-    if (color) {
-        document.getElementById('player').style.borderColor = color;
-    }
-}
+/* イベントリスナ */
 
-function onPlayerStateChange(event) {
-    //changeBorderColor(event.data);
-    //socket.emit('player status', event.data);
-    playerStatus = event.data;
+//PeerJsサーバと接続完了した場合に発生
+peer.on('open', function(id) {
+    document.getElementById("myId").innerHTML = id;
+});
 
-    // Event Listeners
-    switch (playerStatus) {
-        case 0:
-            // Video Ended
-            // Go to next in queue
-            if (host) {
-                playNext(roomnum)
-            }
-            break;
-        case 1:
-            console.log(host)
-            if (host) {
-                playOther(roomnum)
-            } else {
-                getHostData(roomnum)
-            }
-            break;
-        case 2:
-            if (host) {
-                pauseOther(roomnum)
-            }
-            break;
-        case 3:
-            var currTime = player.getCurrentTime();
-            if (host) {
-                seekOther(roomnum, currTime)
-                // syncVideo(roomnum)
-            }
-            break;
-    }
-
-}
-
-function play() {
-    if (playerStatus == -1 || playerStatus == 2) {
-        player.playVideo();
-    } else {
-        player.pauseVideo();
-    }
-}
-
-socket.on('get title', function(data, callback) {
-    var videoId = data.videoId
-    var user = data.user
-
-    $.get(
-        "https://www.googleapis.com/youtube/v3/videos", {
-            part: 'snippet',
-            id: videoId,
-            key: "AIzaSyCuKhQw-AouTjuiEIKquFiJuiWgpffr-LM"
-        },
-        function(data) {
-            // enqueueNotify(user, data.items[0].snippet.title)
-            socket.emit('notify alerts', {
-                alert: 0,
-                user: user,
-                title: data.items[0].snippet.title
-            })
-            // Does a callback and returns title
-            callback({
-                videoId: videoId,
-                title: data.items[0].snippet.title
-            })
-        }
-    )
-})
-
-socket.on('get playlist videos', function(data) {
-    var playlistId = data.playlistId
-    var user = data.user
-
-    $.get(
-        "https://www.googleapis.com/youtube/v3/playlistItems", {
-            part: 'snippet,contentDetails',
-            playlistId: playlistId,
-            maxResults: '50',
-            key: data.api_key
-        },
-        function(data) {
-          // Iterate through all of the playlist videos
-          for (let video of data.items) {
-            enqueueVideo(roomnum, video.contentDetails.videoId)
-          }
-        }
-    )
-})
+//相手のホストと接続完了した時に発生 接続ボタンを押したホストは実行しない
+peer.on('connection', function(con) {
+    showOnStatus();
+    conn = con;
+    conn.on('data', function(data) {
+        receivedContent(data);
+        //console.log("received: " + data);
+    });
+    
+});
