4016d612bd9199afb14959b092bf3fa6554f76e2
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-12 07:36:18 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -154,7 +154,7 @@ socket.on('connect', function(data) {
     //clearing previous setTimeout function.
     clearTimeout(setTime);
     //showing typing message.
-    $('#conversation').append('<span class="ui small text" id="typing">' + '' + '</span>').text(message);
+    $('#typing').append('<span class="ui small text" id="typing">' + '' + '</span>').text(message);
     //showing typing message only for few seconds.
     setTime = setTimeout(function(){
       $('#typing').empty();
