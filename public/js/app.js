3717919126c001275608d65325f5ffb69ccc4f14
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-14 07:00:57 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -86,12 +86,13 @@ var socket = io.connect('anomic.io/');
   // });
   
   socket.on('updateusers', function(data) {
-    var clients = findClientsSocket(roomId);
+    var clients = findClientsSocket(room);
 		$('#userlist').empty();
     $('#userlist').append('<div class="list-group-item-heading"><span class="ui text">' + 'USERS' + '&nbsp;#' + '' + socket.room + '</span></div>');
 		$.each(data, function(key, value) {
 			$('#userlist').append('<div id="connecteduser">' + key + '&nbsp;&nbsp;' + '<i class="small circle icon green"></i><div class="ui mini button"id="PMbutton"><span class="ui medium blue text">PM</span></div></div>');
-      
+      console.log(findClientsSocket(room));
+      console.log(room);
       
       function addBack(){
       $('#conversation').append('<span class="ui small white text"id="messagingthem">Messaging' + key + '</span>')
