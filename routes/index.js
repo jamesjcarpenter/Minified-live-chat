f7c306accee1286fcef9f65ddefa11e85b9115eb
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-13 18:51:21 -0700

    .

--- routes/index.js
+++ routes/index.js
@@ -75,35 +75,35 @@ router.get('/home', function(req, res) {
 
 
 
-router.use(function (req, res, next) {
-
-    if (req.originalUrl === '/users/login') {
-    return next();
-  } if (req.originalUrl === '/home') {
-    return next(); 
-  } if (req.originalUrl === '/room') {
-    return next(); 
-  } if (req.originalUrl === '/users/register') {
-    return next(); 
-  } if (req.originalUrl === '/profile') {
-    return next();  
-  } if (req.originalUrl === '/logout') {
-    return next();  
-  } if (req.originalUrl === '/dashboard') {
-    return next();  
-  } if (req.originalUrl === '/public') {
-    return next();
-  } if (req.originalUrl === '/views') {
-    return next();
-  } if (req.originalUrl === '/newroom') {
-    return next();
-  } if (req.originalUrl === '/') {
-    return next();
-  } else {
-    res.status(404)        // HTTP status 404: NotFound
-   .send('Not found');
-  };
-});
+// router.use(function (req, res, next) {
+// 
+//     if (req.originalUrl === '/users/login') {
+//     return next();
+//   } if (req.originalUrl === '/home') {
+//     return next(); 
+//   } if (req.originalUrl === '/room') {
+//     return next(); 
+//   } if (req.originalUrl === '/users/register') {
+//     return next(); 
+//   } if (req.originalUrl === '/profile') {
+//     return next();  
+//   } if (req.originalUrl === '/logout') {
+//     return next();  
+//   } if (req.originalUrl === '/dashboard') {
+//     return next();  
+//   } if (req.originalUrl === '/public') {
+//     return next();
+//   } if (req.originalUrl === '/views') {
+//     return next();
+//   } if (req.originalUrl === '/newroom') {
+//     return next();
+//   } if (req.originalUrl === '/') {
+//     return next();
+//   } else {
+//     res.status(404)        // HTTP status 404: NotFound
+//    .send('Not found');
+//   };
+// });
 
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
