a7970b335ea4ccd5e9363a9106f351c199e73b4a
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-13 18:14:56 -0700

    .

--- public/js/youtubeapi.js
+++ public/js/youtubeapi.js
@@ -19,11 +19,7 @@ $(function() {
           $.each(results.items, function(index, item) {
             $.get("views/layouts/layout.handlebars", function(data) {
                 $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
-                
-                var myName;
-                  var myTemplate = $('#myTempId').html();
-                  var compiled = Handlebars.compile(myTemplate);
-                
+                  
             });
           });
           resetVideoHeight();
@@ -37,13 +33,41 @@ function resetVideoHeight() {
     $(".video").css("height", $("#results").width() * 9/16);
 }
 
+        var tag = document.createElement('script');
+
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        var player;
+        function onYouTubeIframeAPIReady() {
+            player = new YT.Player('player', { // "player" id of youtube player container where video comes using youtube iframe api.
+                height: '390',
+                width: '640',
+                videoId: 'M7lc1UVf-VE',
+                events: {
+                    'onReady': onPlayerReady, // on ready event below callback function "onPlayerReady" will be called.
+                }
+            });
+        }
+
+        function onPlayerReady(event) { // as youtube player will ready
+            $('#play_vid').click(function() {  // it will wait to click on overlay/image
+                event.target.playVideo();  // as overlay image clicked video plays.
+            });
+        }
+
+        $(document).ready(function() {
+            $('#player').hide(); // on document ready youtube player will be hiden.
+            $('#play_vid').click(function() {  // as user click on overlay image.
+                $('#player').show();    // player will be visible to user 
+                $('#play_vid').hide(); // and overlay image will be hidden.
+            });
+        });
+
+
 function init() {
     gapi.client.setApiKey("AIzaSyCuKhQw-AouTjuiEIKquFiJuiWgpffr-LM");
     gapi.client.load("youtube", "v3", function() {
     });
   }
-
-  $('#youtubebutton').click(function(){
-    url = $('urlId').val();
-    $("#results").append(tplawesome(data, [{"vidurl": url}]));
-  });
