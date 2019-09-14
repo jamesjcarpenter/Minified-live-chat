12bbb6ed64a665f7ee22e74d5f944ffdfca6e176
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-13 23:24:57 -0700

    .

--- public/js/youtubeapi.js
+++ public/js/youtubeapi.js
@@ -23,8 +23,6 @@ $(function() {
                 
                 $.get("views/index.ejs", function(data) {
                   $('#youtubevideo').click( function() {
-                    $("body").append('<%= videoid %>', {videoid: item.id.videoId});
-                     $("#vidWtch").html("<iframe id='youtubeplayer' class='video w100' width='170' height='140' src='//www.youtube.com/embed/<%= videoid %>?rel=0' frameborder='0' allowscriptaccess='always' allowfullscreen></iframe>");
                      });
                 });
             });
