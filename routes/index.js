3bbd054f33eb3ba5653bc8e7aeb0ff2ca54d9548
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-14 02:21:52 -0700

    .

--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,7 @@ router.get('/room', function(req, res) {
   res.locals.query = req.query;
    res.locals.url   = req.originalUrl;
    var videoid;
-   res.render('index.ejs', { videoid: item.id.videoId, error: false, styleNonce: res.locals.styleNonce, name: req.params.name, chat: req.session.chat, username: req.user});
+   res.render('index.ejs', { videoid: videoId, error: false, styleNonce: res.locals.styleNonce, name: req.params.name, chat: req.session.chat, username: req.user});
 });
 
 router.get('/profile', function(req, res) {
