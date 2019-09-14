0be45c0122026ed6c111e11f99633b8bc2a78e74
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-13 20:50:16 -0700

    .

--- public/js/youtubeapi.js
+++ public/js/youtubeapi.js
@@ -17,7 +17,7 @@ $(function() {
           var results = response.result;
           $("#results").html("");
           $.each(results.items, function(index, item) {
-            $.get("views/layouts/layout.handlebars", function(data) {
+            $.get("views/index.ejs", function(data) {
                 $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
             });
           });
