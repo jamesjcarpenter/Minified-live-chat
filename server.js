3989c94b4b4721795152352f83f5e4c380524247
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-14 09:28:33 -0700

    .

--- server.js
+++ server.js
@@ -254,8 +254,9 @@ app.use('/users', users);
 app.use('/room', routes, users);
 
 app.get('/', function (req, res, next) {
-  fs.readFile('/server.js', function (err, data) {
+  fs.readFile('server.js', function (err, data) {
     res.status(404).send("Sorry can't find that!")
+    next();
     });
   });
 
