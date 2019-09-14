3e89d943df3eb8a0fd91215ab137cf0527539705
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-13 17:24:18 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -53,7 +53,7 @@ function ytVids()
 { 
    document.getElementById('youtubeplayer').onclick = function()
    {
-       $('#youtubeplayer').remove();
+       $('.ytp-cued-thumbnail-overlay-image').remove();
    }
 }
 
