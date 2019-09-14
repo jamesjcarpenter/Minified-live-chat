49fc6170db8c9583b37aa12e2b1dfd7c757a92ec
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-14 05:48:02 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -104,13 +104,7 @@ var socket = io.connect('anomic.io/');
          $('#messages').hide();
          $('#conversation').append('<span class="ui small white text"id="messagingthem">Messaging' + key + '</span>')
          $('#conversation').append('<div class="ui mini button"id="goback">go back</div>');
-         $('#data').keypress(function(e) {
-           if(e.which == 13) {
-             $(this).blur();
-             $('#datasend').focus().click();
-             socket.emit('private-message', message);
-           }
-         });
+         socket.emit('private-message', message);
          //
           $('#goback').click(function() {
              $('#messages').show();
