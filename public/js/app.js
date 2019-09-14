b4576578f443666ae00c3e48a33473f7ccf638a7
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-14 07:47:42 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -90,7 +90,6 @@ var socket = io.connect('anomic.io/');
     $('#userlist').append('<div class="list-group-item-heading"><span class="ui text">' + 'USERS' + '&nbsp;#' + '' + socket.room + '</span></div>');
 		$.each(data, function(key, value) {
 			$('#userlist').append('<div id="connecteduser">' + key + '&nbsp;&nbsp;' + '<i class="small circle icon green"></i><div class="ui mini button"id="PMbutton"><span class="ui medium blue text">PM</span></div></div>');
-      $('#connectnow').append($('<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"></img><tag id="username"name="avatar"><span class="ui small text"><samp>' + key + '</samp></span></tag>').text(key));
       
       
       function addBack(){
@@ -190,6 +189,7 @@ socket.on('connect', function(data) {
   
   socket.on('updateroomusers', function(roomusers, username) {
   $("#roomusers").empty();
+        $('#connectnow').append($('<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"></img><tag id="username"name="avatar"><span class="ui small text"><samp>' + username + '</samp></span></tag>').text(username));
   $.each(roomusers, function (key, value) {
   $('#roomusers').append('+value+');
   });
