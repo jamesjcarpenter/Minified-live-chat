af1ee703266080a90b6d0006332e540b55aa29ca
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-14 00:02:36 -0700

    .

--- public/js/youtubeapi.js
+++ public/js/youtubeapi.js
@@ -22,10 +22,12 @@ $(function() {
                 
                 $.get("views/index.ejs", function(data) {
                   $('#youtubevideo').click( function() {
-                    $('#youtubeplayer').appendTo('#vidWtch');
-                    document.getElementById("#youtubeplayer").src="https://www.youtube.com/embed/<%= videoid %>&?autoplay=1?rel=0";
-                    document.getElementById("#youtubeplayer").style.width="360";
-                    document.getElementById("#youtubeplayer").style.height="640";
+                      var youtubeplay = "<iframe id='youtubeplayer' class='video w100' width='170' height='140' src='//www.youtube.com/embed/<%= videoid %>?rel=0' frameborder='0' allowscriptaccess='always' allowfullscreen></iframe>";
+                    $("#vidWtch").html(youtubeplay);
+                    $(youtubeplay).appendTo('#vidNow');
+                  $(youtubeplay).src="https://www.youtube.com/embed/<%= videoid %>&?autoplay=1?rel=0";
+                    $(youtubeplay).style.width="360";
+                    $(youtubeplay).style.height="640";
                      });
                 });
             });
