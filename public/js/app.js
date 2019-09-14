8cec08e6856b06c24b5814d16a3a6a8c79b02927
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-14 07:25:42 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -90,7 +90,8 @@ var socket = io.connect('anomic.io/');
     $('#userlist').append('<div class="list-group-item-heading"><span class="ui text">' + 'USERS' + '&nbsp;#' + '' + socket.room + '</span></div>');
 		$.each(data, function(key, value) {
 			$('#userlist').append('<div id="connecteduser">' + key + '&nbsp;&nbsp;' + '<i class="small circle icon green"></i><div class="ui mini button"id="PMbutton"><span class="ui medium blue text">PM</span></div></div>');
-      
+      $('#connecteduser').empty();
+      $('#connecteduser').append($('<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"></img><tag id="username"name="avatar"><span class="ui small text"><samp></samp></span></tag>').text(username));
       
       
       function addBack(){
