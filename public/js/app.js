d55fd2c708517a15494eed9c6153d77cb2e7c3df
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 07:44:15 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -163,7 +163,15 @@ var socket = io.connect('anomic.io/');
     text = text.replace(':)', '&#9786;').replace('&lt;3', '&#9829;');
     $("#data").html(text);
       $("#message").replaceWith("<div class='ui left pointing label'id='emojimsg'><img id='smileImg' src='images/images/smile.png' /></div>");
-});
+    });
+  
+    $(function() {
+    var text = $("#data").html();
+    text = text.replace(':D', '&#9786;').replace('&lt;3', '&#9829;');
+    $("#data").html(text);
+      $("#message").replaceWith("<div class='ui left pointing label'id='emojimsg'><img id='joyImg' src='images/images/joy.png' /></div>");
+    });
+    
   });
   
 
