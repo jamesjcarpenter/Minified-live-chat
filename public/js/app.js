9bdc40d41bc4434a07d3cc62844bde221f1f82dc
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 11:11:29 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -183,7 +183,7 @@ socket.on('connect', function(data) {
       var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
       var str = '';
       if (re.test(message)) {
-        $(message).each(function(){
+        $('#message').(function(){
             $(this).wrapInner('<a name="' + $(this).appendTo('#conversation') + '" />');
         });
       };
