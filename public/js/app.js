5e61783c04e4617f398b6976a24557086f133629
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 14:02:02 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -180,7 +180,6 @@ socket.on('connect', function(data) {
       
       // document.getElementById("#data").value = '<div class="ui left pointing label"id="emojimsg"><img id="joyImg" src="images/images/joy.png" /></div>'
       var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
-      var str = '' + msgUrl;
       if (re.test(message)) {
               $("#message").each(function(){
                 $(this).html( $(this).html().replace(/((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/, '<a href="$1">$1</a> ') );
