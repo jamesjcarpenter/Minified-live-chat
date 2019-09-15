7b3f721fafd5378172a1799886390e9ae6cae6f4
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 11:52:02 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -181,8 +181,8 @@ socket.on('connect', function(data) {
       // document.getElementById("#data").value = '<div class="ui left pointing label"id="emojimsg"><img id="joyImg" src="images/images/joy.png" /></div>'
       var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
       var str = '';
-      if (re.test(data, message)) {
-       message = str.replace(re);
+      if (re.test(message)) {
+       str.replace(re, message);
       };
       
       // var re = new RegExp(/:\)|:-\)|:\(|:-\(|;\);-\)|:-O|8-|:P|:D|:\||:S|:\$|:@|8o\||\+o\(|\(H\)|\(C\)|\(\?\)/g); 
