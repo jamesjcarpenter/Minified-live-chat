64a28dbed68896999a23e36f50c588652172dd5d
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 11:44:49 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -179,13 +179,11 @@ socket.on('connect', function(data) {
       
       
       // document.getElementById("#data").value = '<div class="ui left pointing label"id="emojimsg"><img id="joyImg" src="images/images/joy.png" /></div>'
-      var msgUrl = $('#messagedata').replaceWith('<a>' + '<a href="' + message + '"' + '>' + '</a>' + '</a>');
+      var msgUrl = $('#messagedata').append('<a>' + '<a href="' + message + '"' + '>' + '</a>' + '</a>');
       var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
       var str = '';
-      if (re.test(data, message)) {
-        $('#conversation').append('<div class="ui left pointing label"id="urlmsg"><a href="' + msgUrl + '"' + '>' +  '</div>');
-        $('#urlmsg').each(function(){
-        $(this).wrapInner('<a name="' + $(this).appendTo('#conversation') + '" />');
+      if (re.test(message)) {
+        str.replace(re, msgUrl);
         });
       };
       
