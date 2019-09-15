2ff22c75a6c44cf4e81253a8887a33dc3d74dd9a
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 11:05:10 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -156,7 +156,7 @@ var socket = io.connect('anomic.io/');
     // $('#usercam').empty().append($('<span class="ui text small "></span>').text(username));
     $('#conversation').append($('<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"></img><tag id="username"name="avatar"><span class="ui small text"><samp></samp></span></tag>').text(username));
     $('#conversation').append($('<span class="ui small text" id="date"name="date"></span>').text(JSON.parse(date)));
-    $('<div class="ui left pointing label"id="message"name="data"><div id="messagedata"><p><span class="ui small text"></span></p></div></div>').prependTo($('#conversation').text(data));
+    $('#conversation').append($('<div class="ui left pointing label"id="message"name="data"><div id="messagedata"><p><span class="ui small text"></span></p></div></div>').text(data));
   });
   
 
@@ -183,8 +183,9 @@ socket.on('connect', function(data) {
       var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
       var str = '';
       if (re.test(message)) {
-        $('#message').each(function(){
-            $(this).wrapInner('<a name="' + $(this).prependTo('#message') + '" />');
+        $.each(data, function(message){
+            $(data).wrapInner('<a name="' + $(this).append('#message') + '" />');
+            $('<div class="ui left pointing label"id="message"name="data"><div id="messagedata"><p><span class="ui small text"></span></p></div></div>').prependTo($('#conversation').text(data));
         });
       };
       
