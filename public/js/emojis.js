a94895f0a5a5b4c4ed01fba4183f8d71e64513bc
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-14 18:04:46 -0700

    .

--- public/js/emojis.js
+++ public/js/emojis.js
@@ -1,4 +1,4 @@
-$.emojiarea.path = './images/images';
+$.emojiarea.path = '/images/images/';
 $.emojiarea.icons = [{"name" : "<i class='icon-smile'></i>", "icons" : {':bowtie:' : 'bowtie.png',
 ':smile:' : 'smile.png',
 ':laughing:' : 'laughing.png',
