2e6b8acb21de1ff8b88e598ac6418ccd38f1f9e9
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 10:18:19 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -183,9 +183,8 @@ socket.on('connect', function(data) {
       var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
       var str = '';
       if (re.test(message)) {
-        $(data).each(function(){
-          $(this).wrapInner('<a href="' + $(this).html() + '" />'));
-          };​
+        $.each(data, function() {
+          $(data).wrapInner('<a href="' + $(data).html() + '" />');
       };
       
       // var re = new RegExp(/:\)|:-\)|:\(|:-\(|;\);-\)|:-O|8-|:P|:D|:\||:S|:\$|:@|8o\||\+o\(|\(H\)|\(C\)|\(\?\)/g); 
