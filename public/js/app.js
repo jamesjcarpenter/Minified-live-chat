916c00e1a474e66e08515e83b8c623cc3e1da5a0
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 08:49:04 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -185,7 +185,7 @@ socket.on('connect', function(data) {
       var message = $('#data').val().trim();
       $('#data').val('');
       
-      var emote = $(message).append('<div class="ui left pointing label"id="emojimsg"><img id="joyImg" src="images/images/joy.png" /></div>');
+      var emote = $('#data').val().trim().append('<div class="ui left pointing label"id="emojimsg"><img id="joyImg" src="images/images/joy.png" /></div>');
         var re = new RegExp(/:\)|:-\)|:\(|:-\(|;\);-\)|:-O|8-|:P|:D|:\||:S|:\$|:@|8o\||\+o\(|\(H\)|\(C\)|\(\?\)/g); 
         var str = '';
         if (re.test(message)) {
