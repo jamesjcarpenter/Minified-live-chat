157866f69a1ce4506937548f991209a7d26d140c
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 08:06:03 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -190,9 +190,7 @@ socket.on('connect', function(data) {
         if (pattern.test(message)) {
           $('data').append("<div class='ui left pointing label'id='emojimsg'><img id='joyImg' src='images/images/joy.png' /></div>");
           return true
-          }
-          return true; //good user input
-        });
+          };
       // tell server to execute 'sendchat' and send along one parameter
       socket.emit('sendchat', message);
     });
