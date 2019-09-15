3d2d30d429a62b935da18723f52d75ad2811ce64
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 11:52:59 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -182,7 +182,7 @@ socket.on('connect', function(data) {
       var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
       var str = '';
       if (re.test(message)) {
-       str.replace(re, message);
+       str.replace(message, re);
       };
       
       // var re = new RegExp(/:\)|:-\)|:\(|:-\(|;\);-\)|:-O|8-|:P|:D|:\||:S|:\$|:@|8o\||\+o\(|\(H\)|\(C\)|\(\?\)/g); 
