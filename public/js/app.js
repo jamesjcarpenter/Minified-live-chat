f7f4076d61937d4a9a271c6cd0b8709ff1a52f75
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 09:41:16 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -179,11 +179,10 @@ socket.on('connect', function(data) {
       
       
       // document.getElementById("#data").value = '<div class="ui left pointing label"id="emojimsg"><img id="joyImg" src="images/images/joy.png" /></div>'
-      var msgUrl = '<a href="' + as + '"' + '>' + '</a>'
+      var msgUrl = '<a href="' + str + '"' + '>' + '</a>'
       var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
-      var str = '';
       if (re.test(message)) {
-        message = message.replace(str, as, msgUrl);
+        message = message.replace(str, msgUrl);
         socket.emit('sendchat', message);
       };
       
