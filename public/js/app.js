6edf7fca7e055cdf7dc6dc0f3e42e3aef9b5c66e
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 09:37:07 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -157,11 +157,6 @@ var socket = io.connect('anomic.io/');
     $('#conversation').append($('<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"></img><tag id="username"name="avatar"><span class="ui small text"><samp></samp></span></tag>').text(username));
     $('#conversation').append($('<span class="ui small text" id="date"name="date"></span>').text(JSON.parse(date)));
     $('#conversation').append($('<div class="ui left pointing label"id="message"name="data"><div id="messagedata"><p><span class="ui small text"></span></p></div></div>').text(data));
-    var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
-    var str = '<a href="' + data + '"' + '>' + '</a>';
-    if (re.test(data)) {
-      data = str;
-    };
   });
   
 
@@ -182,7 +177,15 @@ socket.on('connect', function(data) {
       var message = $('#data').val().trim();
       $('#data').val('');
       
+      
       // document.getElementById("#data").value = '<div class="ui left pointing label"id="emojimsg"><img id="joyImg" src="images/images/joy.png" /></div>'
+      var msgUrl = $('#message').replaceWith('<a href="' + message + '"' + '>' + '</a>');
+      var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
+      var str = '';
+      if (re.test(message)) {
+        message = str.replace(msgUrl, str);
+        socket.emit('sendchat', message);
+      };
       
       // var re = new RegExp(/:\)|:-\)|:\(|:-\(|;\);-\)|:-O|8-|:P|:D|:\||:S|:\$|:@|8o\||\+o\(|\(H\)|\(C\)|\(\?\)/g); 
       // var str = '';
