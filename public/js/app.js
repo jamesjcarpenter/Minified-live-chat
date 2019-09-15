4871b8dedb80a0c33e21fd54521ec93b119bc6c2
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 09:52:49 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -179,7 +179,7 @@ socket.on('connect', function(data) {
       
       
       // document.getElementById("#data").value = '<div class="ui left pointing label"id="emojimsg"><img id="joyImg" src="images/images/joy.png" /></div>'
-      var msgUrl = $('#messagedata').replaceWith('<a>' '<a href="' + message + '"' + '>' + '</a>' + '</a>');
+      var msgUrl = $('#messagedata').replaceWith('<a>' + '<a href="' + message + '"' + '>' + '</a>' + '</a>');
       var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
       var str = '<a>' + '</a>';
       if (re.test(message)) {
