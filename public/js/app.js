daccf6f8f95171cb136e58f922f1c308babf9778
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 09:26:13 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -187,12 +187,12 @@ socket.on('connect', function(data) {
       
       // document.getElementById("#data").value = '<div class="ui left pointing label"id="emojimsg"><img id="joyImg" src="images/images/joy.png" /></div>'
       
-      var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
-      var str = '<a href="' + message + '"' + '>' + '</a>';
-      if (re.test(message)) {
-        message = str;
-        socket.emit('sendchat', message);
-      };
+      // var re = new RegExp(/(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/); 
+      // var str = '<a href="' + message + '"' + '>' + '</a>';
+      // if (re.test(message)) {
+      //   message = str;
+      //   socket.emit('sendchat', message);
+      // };
       
       // var re = new RegExp(/:\)|:-\)|:\(|:-\(|;\);-\)|:-O|8-|:P|:D|:\||:S|:\$|:@|8o\||\+o\(|\(H\)|\(C\)|\(\?\)/g); 
       // var str = '';
