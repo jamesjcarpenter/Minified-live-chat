ab07c69828e472aea9092ce33c7d8291b0d9d5b1
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-16 14:39:26 -0700

    save arezz

--- public/js/videocall.js
+++ public/js/videocall.js
@@ -44,9 +44,9 @@
 //
 var server = null;
 if(window.location.protocol === 'http:')
-	server = "http://" + window.location.hostname + ":8088/janus";
+	server = "ws://" + window.location.hostname + ":8088/janus";
 else
-	server = "https://" + window.location.hostname + ":8089/janus";
+	server = "wss://" + window.location.hostname + ":8089/janus";
 
 var janus = null;
 var videocall = null;
@@ -65,17 +65,12 @@ var doSimulcast = (getQueryStringValue("simulcast") === "yes" || getQueryStringV
 var doSimulcast2 = (getQueryStringValue("simulcast2") === "yes" || getQueryStringValue("simulcast2") === "true");
 var simulcastStarted = false;
 
-$(document).ready(function() {
 	// Initialize the library (console debug enabled)
 	Janus.init({debug: true, callback: function() {
 		// Use a button to start the demo
 		$('#start').one('click', function() {
 			// $(this).attr('disabled', false).unbind('click');
 			// Make sure the browser supports WebRTC
-			if(!Janus.isWebrtcSupported()) {
-				bootbox.alert("No WebRTC support... ");
-				return;
-			}
 			// Create session
 			janus = new Janus(
 				{
