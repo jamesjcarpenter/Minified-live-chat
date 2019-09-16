17644a644fb211507fb59ef834bb24e3d53a9304
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-16 02:19:35 -0700

    save arezz

--- janus.js
+++ janus.js
@@ -1734,7 +1734,7 @@ function Janus(gatewayCallbacks) {
 		}
 		// If we still need to create a PeerConnection, let's do that
 		if(!config.pc) {
-			var pc_config = {"iceServers": iceServers, "iceTransportPolicy": 'relay', "bundlePolicy": bundlePolicy};
+			var pc_config = {"iceServers": iceServers, "iceTransportPolicy": iceTransportPolicy, "bundlePolicy": bundlePolicy};
 			if(Janus.webRTCAdapter.browserDetails.browser === "chrome") {
 				// For Chrome versions before 72, we force a plan-b semantic, and unified-plan otherwise
 				pc_config["sdpSemantics"] = (Janus.webRTCAdapter.browserDetails.version < 72) ? "plan-b" : "unified-plan";
