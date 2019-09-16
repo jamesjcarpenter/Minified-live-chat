fe5700cd5c7b24b03ab1ea921313fb25045e19f1
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-16 15:51:08 -0700

    save arezz

--- public/js/videocall.js
+++ public/js/videocall.js
@@ -286,7 +286,7 @@ var simulcastStarted = false;
 												}
 											}
 										}
-									} else {
+									} else  if{
 										// FIXME Error?
 										var error = msg["error"];
 										bootbox.alert(error);
