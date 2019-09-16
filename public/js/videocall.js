85f8538a8cec5e7db6e08f36b146e54a6f3c7e63
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-16 15:38:44 -0700

    save arezz

--- public/js/videocall.js
+++ public/js/videocall.js
@@ -148,7 +148,7 @@ var simulcastStarted = false;
 											}
 										} else if(result["event"] !== undefined && result["event"] !== null) {
 											var event = result["event"];
-											if(event === undefined) {
+											if(event === result["event"]) {
 												myusername = $('#keyUse').attr('name')
 												Janus.log("Successfully registered as " + myusername + "!");
 												$('#youok').removeClass('hide').show().html("Registered as '" + myusername + "'");
