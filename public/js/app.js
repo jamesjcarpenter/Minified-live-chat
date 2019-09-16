01611185c1b2c8212b18abd1020e4fe380a832b2
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-15 20:08:02 -0700

    save arezz

--- public/js/app.js
+++ public/js/app.js
@@ -112,7 +112,6 @@ var socket = io.connect('anomic.io/');
 			$('#userlist').append('<div id="connecteduser"name="' + socket.username + '">' + '<div id="keyUse"name="' + socket.id  + '">' + key + '</div>' + '&nbsp;&nbsp;' + '<i class="small circle icon green"></i><div class="ui mini button pm" id="' + socket.id + '"><span class="ui medium blue text">PM</span></div></div>');
     });
   });
-    });
       // function addBack(){
     //   $('#conversation').append('<span class="ui small white text"id="messagingthem">Messaging' + key + '</span>')
     //   $('#conversation').append('<div class="ui mini button"id="goback">go back</div>');
@@ -438,3 +437,4 @@ designer.widgetJsURL = 'https://www.webrtc-experiment.com/Canvas-Designer/widget
 // <iframe> will be appended to "document.body"
 designer.appendTo(document.body.inner|| document.documentElement);
 });
+    });
