fd22230c0c1e436639a0689d2809a545ffef8781
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-17 01:26:56 -0700

    save arezz

--- public/js/videocalltest.js
+++ public/js/videocalltest.js
@@ -728,4 +728,4 @@ function updateSimulcastButtons(substream, temporal) {
 		$('#tl-1').removeClass('btn-primary btn-success').addClass('btn-primary');
 		$('#tl-0').removeClass('btn-primary btn-success').addClass('btn-primary');
 	}
-}
+});
