e0cc08cb1e70e9fe041c3364492d8692ca4f3884
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-16 22:31:43 -0700

    save arezz

--- public/js/app.js
+++ public/js/app.js
@@ -55,7 +55,15 @@ var date = JSON.stringify(new Date(Date.now()).toLocaleTimeString())
 var socket = io.connect('anomic.io/');
   // Add validation rules to Create/Join Room Form
   socket.on('connect', function(){
-    		socket.emit('adduser', prompt("Enter username."));
+    socket.room = {};
+    var roomId = url.substr(url.lastIndexOf("=")+1);
+    socket.room = roomId;
+    // console.log("roomId : "+roomId);
+    //event to get chat history on button click or as room is set.
+     socket.emit('join', roomId);
+  
+  // call the server-side function 'adduser' and send one parameter (value of prompt)
+  socket.emit('adduser', prompt("Enter username."));
       //empty messages.
       socket.on('askForUserId', () => {
         socket.emit(socket.id);
@@ -65,15 +73,6 @@ var socket = io.connect('anomic.io/');
         console.log('You received a message');
         console.log(message.data);
         });
-      
-      socket.room = {};
-      var roomId = url.substr(url.lastIndexOf("=")+1);
-      socket.room = roomId;
-      // console.log("roomId : "+roomId);
-      //event to get chat history on button click or as room is set.
-       socket.emit('join', roomId);
-    
-		// call the server-side function 'adduser' and send one parameter (value of prompt)
     //   socket.emit('adduser',
     //   $('.ui.mini.basic.modal.start')
     //   .modal({  
