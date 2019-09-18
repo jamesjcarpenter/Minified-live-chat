b1491a74f6968e99870fecf8a1fdb719e97b8052
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-18 09:33:33 -0700

    adding youtube

--- server.js
+++ server.js
@@ -398,9 +398,6 @@ rooms = [];
 // Store all of the sockets and their respective room numbers
 userrooms = {}
 
-YT3_API_KEY = process.env.YT3_API_KEY
-DM_API_KEY = process.env.DM_API_KEY
-
 // Set given room for url parameter
 var given_room = ""
 
