072f3717ba814fcd746e86e733c1a8ab42092c32
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-17 21:37:11 -0700

    save arezz

--- public/js/streaming.js
+++ public/js/streaming.js
@@ -80,7 +80,7 @@ var simulcastStarted = false, svcStarted = false;
 var selectedStream = null;
 
 window.addEventListener('DOMContentLoaded', (event) => {
-	var Janus;
+	var Janus = null;
 	var streaming = null;
 	// Initialize the library (all console debuggers enabled)
 	Janus.init({debug: "all", callback: function() {
