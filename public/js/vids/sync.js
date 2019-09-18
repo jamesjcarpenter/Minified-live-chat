81c1afe60df18e82dd60138d637e4691ab07854e
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-18 13:10:11 -0700

    adding youtube

--- public/js/vids/sync.js
+++ public/js/vids/sync.js
@@ -78,7 +78,7 @@ function syncVideo(roomnum) {
     switch (currPlayer) {
         case 0:
           ytplayer = $(".video-stream.html5-main-video");
-          ytplayer.getCurrentTime();
+          ytplayer.getTime();
             state = playerStatus
             console.log("I am host and my current time is " + currTime + state)
             break;
