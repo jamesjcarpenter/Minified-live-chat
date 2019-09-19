8687d5c380eb49f064514411735decc00ea5e9a0
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-19 01:11:46 -0700

    .

--- public/js/vids/yt.js
+++ public/js/vids/yt.js
@@ -6,37 +6,35 @@ tag.src = "//www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  // This function creates an <iframe> (and YouTube player)
-  // after the API code downloads.
-  var player;
-  window.onYouTubeIframeAPIReady = function() {
-    player = new YT.Player("player", {
-      "height": "315",
-      "width": "560",
-      "videoId": "bHQqvYy5KYo",
-      "origin": "https://www.anomic.io/",
-      "host": "https://www.youtube.com/",
-      "events": {
-        "onReady": onPlayerReady,
-        "onStateChange": onPlayerStateChange
-      }
-    });
-  }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-        
-      }
-    function onPlayerStateChange(event){
-        if(event.data==1) { // playing
-            myTimer = setInterval(function(){ 
-                var time;
-                time = player.getCurrentTime();
-                $("#timerValue").text(time);
-            }, 100);
-        }
-        else { // not playing
-            clearInterval(myTimer);
-        }
+// This function creates an <iframe> (and YouTube player)
+// after the API code downloads.
+var player;
+window.onYouTubeIframeAPIReady = function() {
+  player = new YT.Player("player", {
+    "height": "315",
+    "width": "560",
+    "videoId": "bHQqvYy5KYo",
+    "events": {
+      "onReady": onPlayerReady,
+      "onStateChange": onPlayerStateChange
     }
+  });
+}
+
+    // 4. The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+      event.target.playVideo();
+      
+    }
+  function onPlayerStateChange(event){
+      if(event.data==1) { // playing
+          myTimer = setInterval(function(){ 
+              var time;
+              time = player.getCurrentTime();
+              $("#timeHolder").text(time);
+          }, 100);
+      }
+      else { // not playing
+          clearInterval(myTimer);
+      }
+  }
