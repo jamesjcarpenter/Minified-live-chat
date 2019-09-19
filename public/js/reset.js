99e5602a97b6f078421735e78dbd13ca2f1a229b
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-19 04:30:02 -0700

    .

---
+++ public/js/reset.js
@@ -0,0 +1,5 @@
+$('.ui.button').addClass('inverted');
+$('.icon').addClass('inverted');
+$('.ui.medium.left.pointing.label').addClass('inverted');
+$('.large.ui.teal.secondary.button.inverted').removeClass('large ui teal').addClass('large ui black');
+$('#cpybutton').removeClass('ui teal').addClass('ui black');
