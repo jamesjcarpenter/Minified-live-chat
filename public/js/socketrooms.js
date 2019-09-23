bba98b3f46628e071e0555bd4f3d69a540674983
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-23 16:51:31 -0700

    updated

---
+++ public/js/socketrooms.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    var socket = io.connect('anomic.io/');
+
+    var values = [];
+    
+    
+    for (var i = 0; i < socket.rooms.length; i++)
+    {
+        var room = socket.rooms[i];
+        if(values.indexOf(room[1] === -1) values.push(room[1]);
+        
+    };
+
+      console.log(values);
+
+
+    socket.on('connect', function(data) {
+      $.each(data, function(key, value) {
+  			$('#roomlist').append('' + '<span class="ui white text">' + key + '</span>' + '<span class="ui white text">' + '' + '</span>' + '<br>');
+    });
+
+});
