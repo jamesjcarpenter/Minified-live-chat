131dcc7ac30ee7f428ab66171551728aafe3f8be
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-23 17:19:23 -0700

    updated

--- server.js
+++ server.js
@@ -389,7 +389,7 @@ app.use((err, req, res, next) => {
 
 
 
-var rooms = ['1','2','3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
+var rooms = {};
 // usernames which are currently connected to the chat
 var usernames = {};
 // rooms which are currently available in chat
