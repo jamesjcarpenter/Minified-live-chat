776ea6497e59b3fdc2b801ea17538dac69c8b1ac
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-23 17:08:59 -0700

    updated

--- public/js/socketrooms.js
+++ public/js/socketrooms.js
@@ -1,8 +1,9 @@
-
-    var socket = io.connect('https://www.anomic.io/443');
-
-    $(document).ready(function() {
-
+var socket = io.connect('anomic.io/');
+  // Add validation rules to Create/Join Room Form
+  
+  
+  socket.on('connect', function(){
+    
     var values = [];
     
     
@@ -14,11 +15,24 @@
     };
 
       console.log(values);
+      
+    $.each(data, function(key, values) {
+      $('#roomlist').append('' + '<span class="ui white text">' + key + '</span>' + '<span class="ui white text">' + '' + values + '</span>' + '<br>');
+    
+      
+    socket.room = {};
+    var roomId = url.substr(url.lastIndexOf("=")+1);
+    socket.room = roomId;
+    // console.log("roomId : "+roomId);
+    //event to get chat history on button click or as room is set.
+     socket.emit('join', roomId);
 
+       socket.emit('adduser', prompt("Enter username."));
 
-    socket.on('connect', function(data) {
-      $.each(data, function(key, value) {
-  			$('#roomlist').append('' + '<span class="ui white text">' + key + '</span>' + '<span class="ui white text">' + '' + '</span>' + '<br>');
-    });
-
-});
+  // call the server-side function 'adduser' and send one parameter (value of prompt)
+      //empty messages.
+      socket.on('askForUserId', () => {
+        socket.emit(socket.id);
+  
+      });
+  });
