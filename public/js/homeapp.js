df4cb06f245dfaa69e8d22770c9897d5e691b8e8
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-23 19:42:06 -0700

    updated

--- public/js/homeapp.js
+++ public/js/homeapp.js
@@ -341,10 +341,10 @@ socket.on('connect', function(data) {
       $('#roomlist').empty();
       $.each(rooms, function(key, value) {
         if(value == current_room){
-          $('#roomlist').append('<div>' + value.substr(value.lastIndexOf("/")+1) + '</div>');
+          $('#roomlist').append('<div>' + value.substr(value.lastIndexOf("/")+1); + '</div>');
         }
         else {
-          $('#roomlist').empty();
+          $('#roomlist').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
         }
       });
     });
