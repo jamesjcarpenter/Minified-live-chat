4470cc75c731a1eec7e690d85a902e4b5ef11614
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-23 17:31:20 -0700

    updated

--- server.js
+++ server.js
@@ -451,12 +451,11 @@ io.sockets.on('connection', function (socket) {
     socket.broadcast.to(socket.room).emit('serverupdatechat', '' + socket.username + ' ' + 'joined the room');
       io.in(socket.room).emit('updateusers', usernames, socket.id);
     // console.log(usernames);
-      io.in(socket.room).emit('updaterooms', rooms, socket.room);
+      // io.in(socket.room).emit('updaterooms', rooms, socket.room);
     
     // socket.broadcast.to(socket.room).emit('addname', socket.username);
     
     socket.on('connect', function(client) {
-        ids.splice(ids.indexOf(socket.id), 1);
 
         socket.on('disconnect', function() {
           ids.splice(ids.indexOf(socket.id), 1);
