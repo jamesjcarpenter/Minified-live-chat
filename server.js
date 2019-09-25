ae204e0d1c1f3293e4d21353414834faebaca6e6
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-25 04:29:31 -0700

    updated

--- server.js
+++ server.js
@@ -416,16 +416,16 @@ io.sockets.on('connection', function (socket) {
     // const ioChat = io.of("/room" + "");
     socket.on('join', function(room) {
       socket.room = room;
-      rooms.push(room);
-      console.log(rooms.push(room));
+      rooms.push('room: ' + room);
+      console.log('room push' + rooms.push(room));
       socket.join(room);
     socket.emit('updaterooms', rooms, socket.room);
     
     for (var i = 0; i < rooms.length; i++) {
     console.log(rooms[i]);
     var curRoom = rooms[i]
-    console.log(rooms[i]);
-    console.log(rooms);
+    console.log('rooms i ' + rooms[i]);
+    console.log('rooms: ' + rooms);
     }
     
     function find_duplicates(rooms) {
@@ -442,10 +442,11 @@ io.sockets.on('connection', function (socket) {
   }
 
   return out;
-  console.log(out);
+  console.log('out' + out);
+  console.log('item' + item);
 }
 
-find_duplicates(rooms); 
+find_duplicates(rooms[i]); 
     
     console.log('testing: ' + Object.keys(socket.room));
       // socket.emit('get host data');
