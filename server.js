e0551fbf2bc05d3e692c675596f389078d4f8f09
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-25 14:28:53 -0700

    updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ requestCert: false,
 rejectUnauthorized: false,
 pingTimeout: 60000,
 },app);
-server.listen(8080);
+server.listen(443);
 var router = express.Router();
 
 //make sure you keep this order
