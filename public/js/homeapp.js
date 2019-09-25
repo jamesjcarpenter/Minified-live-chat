f4b1a9da600495a118dcd0162fbd23b854fa2560
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-25 07:37:21 -0700

    updated

--- public/js/homeapp.js
+++ public/js/homeapp.js
@@ -340,7 +340,7 @@ socket.on('connect', function(data) {
   });
   
   socket.on('updateactive', function(numClients) {
-    $('#globallist').empty();
+    // $('#globallist').empty();
     // $('#userlist').append('<div class="list-group-item-heading"><span class="ui white text">' + 'online' + '<br>' + 'room &nbsp;#' + '' + socket.room + '</span></div><br><br>');
     // $.each(data, function(key, value) {
       $('#globallist').append('' + numClients);
