bb8585e5fec0f1a1b8c573447e4bab121da693f7
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-25 04:05:41 -0700

    updated

--- server.js
+++ server.js
@@ -419,7 +419,7 @@ io.sockets.on('connection', function (socket) {
       rooms.push(room);
       socket.join(room);
     socket.emit('updaterooms', rooms, socket.room);
-    }
+  });
     console.log('testing: ' + Object.keys(socket.room));
       // socket.emit('get host data');
       
@@ -459,7 +459,7 @@ io.sockets.on('connection', function (socket) {
     console.log(rooms[i]);
     var curRoom = rooms[i]
       socket.emit('updaterooms', rooms, socket.room, curRoom);
-    });
+    };
     // socket.broadcast.to(socket.room).emit('addname', socket.username);
     
     socket.on('connect', function(client) {
