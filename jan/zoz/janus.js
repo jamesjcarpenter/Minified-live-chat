21b7cd41f82ea40299a61a4e4f5ce5ed9011856a
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-25 14:21:13 -0700

    updated

--- jan/zoz/janus.js
+++ jan/zoz/janus.js
@@ -26,9 +26,9 @@
 
  var server = null;
  if(window.location.protocol === 'http:')
-         server = "http://" + window.location.hostname + ":8088/janus";
+         server = "http://" + window.location.hostname + ":8080/janus";
  else
-         server = "https://" + window.location.hostname + ":8089/janus";
+         server = "https://" + window.location.hostname + ":8080/janus";
 
 // List of sessions
 Janus.sessions = {};
--- jan/zoz/videoroom.js
+++ jan/zoz/videoroom.js
@@ -44,9 +44,9 @@
 //
 var server = null;
 if(window.location.protocol === 'http:')
-	server = "http://" + window.location.hostname + ":8088/janus";
+				server = "http://" + window.location.hostname + ":8080/janus";
 else
-	server = "https://" + window.location.hostname + ":8089/janus";
+				server = "https://" + window.location.hostname + ":8080/janus";
 
 var janus = null;
 var sfutest = null;
