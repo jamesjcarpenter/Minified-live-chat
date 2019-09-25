2b3c0c7a20656d5d24435d0082071ba8df883de1
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-25 06:05:01 -0700

    updated

--- server.js
+++ server.js
@@ -417,10 +417,11 @@ io.sockets.on('connection', function (socket) {
     socket.on('join', function(room) {
       socket.room = room;
       socket.join(room);
-      if (room.indexOf(rooms) >= 0) {
+      if (room.indexOf(rooms) === 0) {
       rooms.push(room);
     } else {
       return false;
+      console.log(rooms);
     };
     // io.emit('updaterooms', rooms, socket.room);
     io.emit('updatehomepage', rooms, socket.room);
