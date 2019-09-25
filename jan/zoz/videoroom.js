b371f11de0af652ab759d90a125ea136906579b7
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-25 11:50:32 -0700

    updated

--- jan/zoz/videoroom.js
+++ jan/zoz/videoroom.js
@@ -42,7 +42,7 @@
 // in the presented order. The first working server will be used for
 // the whole session.
 //
-var server = "https://165.22.137.67:8089/janus";
+var server = "https://165.22.137.67:10104/janus";
 				 
 				 
 var janus = null;
