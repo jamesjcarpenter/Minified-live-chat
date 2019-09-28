3e808c7cda29780557a5dcb86a2dec4f4c503c4a
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-28 04:46:18 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -41,7 +41,7 @@ $('#userscheck').click( function() {
     // document.getElementById('themecss').href = 'css/indextheme2.css';
     // $('.ui.button').addClass('inverted');
     $('.icon').addClass('inverted');
-    $('.ui.medium.left.pointing.label').addClass('inverted');
+    // $('.ui.medium.left.pointing.label').addClass('inverted');
     // $('.large.ui.teal.secondary.button.inverted').removeClass('large ui teal').addClass('large ui black');
     // $('#cpybutton').removeClass('ui teal').addClass('ui black');
     
