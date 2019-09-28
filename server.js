0d0b9957a228d26f8df629454ad007fa30e36dd4
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-28 04:40:40 -0700

    .

--- server.js
+++ server.js
@@ -464,7 +464,7 @@ io.sockets.on('connection', function (socket) {
     console.log(usernames)
     console.log(ids)
     
-    socket.emit('serverupdatechat', ' ' + socket.username + 'joined the room' + ' ' + socket.room);
+    socket.emit('serverupdatechat', 'you joined the room' + ' ' + socket.room);
     // echo to room 1 that a person has connected to their room
     socket.broadcast.to(socket.room).emit('serverupdatechat', '' + socket.username + ' ' + 'joined the room');
     
