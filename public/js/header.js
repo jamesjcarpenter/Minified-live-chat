11dccebd2a8e2d09f80bb822c208d144a77df4b7
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-29 09:56:35 -0700

    .

--- public/js/header.js
+++ public/js/header.js
@@ -3,7 +3,7 @@ var url = window.location.href;
 var newUrl = "track.omegatrck.com/8e741d3d-7a47-407d-be8d-07da4c800b99?source=test"
 
     function getNum() {
-      if (ranNum === 1) {
+      if (ranNum == 2) {
       window.location.replace("http://track.omegatrck.com/8e741d3d-7a47-407d-be8d-07da4c800b99?source=test");
       console.log('1 rolled');
       } else {
