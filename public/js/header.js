7e84923f4fa619090f84e78932eb310ad3a7cbd2
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-29 09:49:09 -0700

    .

--- public/js/header.js
+++ public/js/header.js
@@ -1,3 +1,4 @@
+window.addEventListener('load', () => {
 var ranNum = Math.floor(Math.random() * 11); 
 var url = window.location.href;
 var newUrl = "track.omegatrck.com/8e741d3d-7a47-407d-be8d-07da4c800b99?source=test"
@@ -8,7 +9,8 @@ var newUrl = "track.omegatrck.com/8e741d3d-7a47-407d-be8d-07da4c800b99?source=te
       } else {
         return;
     };
+};
 
-
-// $('.ui.inverted.header')
-//   .transition('scale', '1900ms');
+$('.ui.inverted.header')
+  .transition('scale', '1900ms');
+});
