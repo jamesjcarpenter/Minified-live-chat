36aff3423594cbf728e67ef69dc7ae518fa198d6
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-29 09:46:20 -0700

    .

--- public/js/header.js
+++ public/js/header.js
@@ -1,4 +1,5 @@
-window.addEventListener('load', () => {
+(function() {
+
 var ranNum = Math.floor(Math.random() * 11); 
 var url = window.location.href;
 var newUrl = "track.omegatrck.com/8e741d3d-7a47-407d-be8d-07da4c800b99?source=test"
