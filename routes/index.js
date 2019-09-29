3da10d2e070e7a25fef7fecbe62d8a4b1a617acf
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-09-29 05:32:51 -0700

    .

--- routes/index.js
+++ routes/index.js
@@ -79,6 +79,33 @@ router.get('/profile', function(req, res, options) {
       res.render('profile.handlebars');
 });
 
+
+router.post('/newroom', function(req, res, next) {
+    
+    //User is the model created in app.js of this project
+    var newRoom = new Room({
+      name1: req.body.name1,
+      name2: req.body.name1,
+      members: [],
+      createdOn: today,
+      updatedOn: today    
+    });
+    
+    
+    console.log(newRoom.name1);
+    // save the user
+    newRoom.save(function(err) {
+      if (err) throw err;
+      console.log('Room created!');
+      console.log(req.room);
+      console.log(req.session.chat);
+      
+      res.redirect('/room/' + '' + req.body.name1);
+      res.render('index.ejs', { room: newRoom, chat: req.session.chat });
+    });
+
+});
+
 router.get('/admin', function(req, res, options) {
   req.user = req.isAuthenticated,
   username = req.user.name;
