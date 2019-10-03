5b0461727eefc5a0f740953fdf697e0dea151139
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-10-02 20:54:45 -0700

    .

--- public/js/transin.js
+++ public/js/transin.js
@@ -1,9 +1,10 @@
-// window.addEventListener('load', () => {
-//   document.getElementsByTagName("body")[0].style.visibility = "visible";
-//    $("body")
-//     .transition({
-//       animation  : 'fade in',
-//       duration:2100,
-//       interval:0
-//      });
-// });
+window.addEventListener('load', () => {
+  document.getElementsByTagName("body")[0].style.visibility = "visible";
+   $("body")
+    .transition({
+      animation  : 'fade in',
+      duration:2100,
+      interval:0
+     });
+     $(".h6:first-child").remove();
+});
