2124af5accb315090f443307b4e7106e956cbd92
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-10-02 23:50:19 -0700

    .

--- server.js
+++ server.js
@@ -398,7 +398,7 @@ var clients = [];
 var users = [];
 var roomAdmin;
 var ids = {};
-
+var id;
 process.env.YT3_API_KEY = 'AIzaSyCuKhQw-AouTjuiEIKquFiJuiWgpffr-LM';
 process.env.VM_API_KEY = 'biQnjEMy7RqMV1Tn37VhPAWxVF7411gbSiglfICUAAaeCwFX1+Gy/HqI4vOe6dYy2qfgAR4qzwqe4guVnUio3ptnObAcqCHseywHAu+EoElpc4bbH88cpDdRQFmx2hAI';
 process.env.DM_API_KEY = '3b47b316af2962e6c94c';
