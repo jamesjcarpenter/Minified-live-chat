0d00a1b876057e7efb5cd1f175c7f10c4f04d7bd
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-10-02 20:48:58 -0700

    .

--- public/js/transin.js
+++ public/js/transin.js
@@ -1,9 +1,9 @@
-window.addEventListener('load', () => {
-  document.getElementsByTagName("body")[0].style.visibility = "visible";
-   $("body")
-    .transition({
-      animation  : 'fade in',
-      duration:2100,
-      interval:0
-     });
-});
+// window.addEventListener('load', () => {
+//   document.getElementsByTagName("body")[0].style.visibility = "visible";
+//    $("body")
+//     .transition({
+//       animation  : 'fade in',
+//       duration:2100,
+//       interval:0
+//      });
+// });
--- public/js/transitions.js
+++ public/js/transitions.js
@@ -1,19 +1,19 @@
-window.addEventListener('load', () => {
-   $(".pusher")
-    .transition({
-      animation  : 'scale in',
-      duration:700,
-      interval:0
-     });
-       
-     $('.scroll.container .ui.container')
-     .visibility({
-       once: false,
-       // update size when new content loads
-       observeChanges: true,
-       // load content on bottom edge visible
-       onBottomVisible: function() {
-         // loads a max of 5 times
-       }
-     });
-  });
+// window.addEventListener('load', () => {
+//    $(".pusher")
+//     .transition({
+//       animation  : 'scale in',
+//       duration:700,
+//       interval:0
+//      });
+// 
+//      $('.scroll.container .ui.container')
+//      .visibility({
+//        once: false,
+//        // update size when new content loads
+//        observeChanges: true,
+//        // load content on bottom edge visible
+//        onBottomVisible: function() {
+//          // loads a max of 5 times
+//        }
+//      });
+//   });
