9b992cc03d386cc8022ff206c4e8fb605a479d45
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-10-03 06:21:23 -0700

    .

--- public/js/app.js
+++ public/js/app.js
@@ -147,20 +147,6 @@ var date = JSON.stringify(new Date(Date.now()).toLocaleTimeString())
     // $('#userlist').append('<div class="list-group-item-heading"><span class="ui white text">' + 'online' + '<br>' + 'room &nbsp;#' + '' + socket.room + '</span></div><br><br>');
 		$.each(data, function(key, value) {
 			$('#userlist').append('<li><a class="ui gray circular image label"id="imagelabel"><img src="/images/avatarsmall.jpg">' + '<span class="ui white text user">' + key  + '</span>' + '</a>' + '</li>');
-      var $container = $('.ui.gray.circular.image.label');
-      $('<div class="ui mini button pm"id="pmbtn"></div>').data('id', socket.id).appendTo($container);
-    
-      console.log('socket id for user' + $('.ui.mini.button.pm').data('id'));
-      
-      
-      
-      $('.ui.mini.button.pm').click(function() {
-        // $("#PMbutton").unbind();
-        $("#pmbtn").each(function(){
-          var value;
-          value = $('#pmbtn').data( "id" );
-          
-          console.log(value);
                 
                 
           console.log(userToPM);
@@ -254,6 +240,23 @@ socket.on('serverupdateuser', function (server) {
   $('#profilepic2').empty();
   $('#profilepic2').append('<h5 class="ui text"id="profilepic2"></h5>').text(server);
   console.log($('.ui.mini.button.pm').attr('name'))
+  
+  var $container = $('.ui.gray.circular.image.label');
+  $('<div class="ui mini button pm"id="pmbtn"></div>').data('id', socket.id).appendTo($container);
+
+  console.log('socket id for user' + $('.ui.mini.button.pm').data('id'));
+  
+  
+  
+  $('.ui.mini.button.pm').click(function() {
+    // $("#PMbutton").unbind();
+    $(".ui.mini.button.pm").each(function(){
+      var value;
+      value = $('.ui.mini.button.pm').data( "id" );
+      
+      console.log(value);
+    });
+  });
   
 });
 
