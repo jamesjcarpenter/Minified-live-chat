2f98d9c14421fcc6ad27dd18cff6c2a5b99dd4fa
spacesvchost <50810940+spacesvchost@users.noreply.github.com>
2019-10-04 06:29:29 -0700

    .

--- server.js
+++ server.js
@@ -491,7 +491,7 @@ io.sockets.on('connection', function (socket) {
     var userNameOfUserToFind;
     Connect.findOne({client : userNameOfUserToFind}).exec(function(err,res) {
     if(res!=null)
-        io.to(res.`${socketId}`).emit('my message', msg);
+        io.to(res.${socketId}).emit('my message', msg);
       })
     });
 
