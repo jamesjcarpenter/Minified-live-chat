1a9aaff4e922670566426b7141e74d89ccefdba5
Your Name <you@example.com>
2019-11-04 07:07:51 -0500

    .

--- public/js/app.js
+++ public/js/app.js
@@ -95,12 +95,12 @@ var date = JSON.stringify(new Date(Date.now()).toLocaleTimeString())
           var objReturn = JSON.stringify(userReturn)
           objReturn =  objReturn.replace(/\\"/g,"\uFFFF"); //U+ FFFF
           objReturn = objReturn.replace(/\"([^"]+)\":/g,"$1:").replace(/\uFFFF/g,"\\\"");
-          socket.username = objReturn;
           $('.ui.mini.basic.modal.start').modal('hide')
         }
     
     
       }).modal('show'));
+      socket.username = objReturn;
     });
 
     socket.room = [];
