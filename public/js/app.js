7166d0ff2f86809ef1bbeb9384f52d8b7648ea97
Your Name <you@example.com>
2019-11-04 08:08:11 -0500

    .

--- public/js/app.js
+++ public/js/app.js
@@ -89,14 +89,13 @@ var date = JSON.stringify(new Date(Date.now()).toLocaleTimeString())
     //event to get chat history on button click or as room is set.
      socket.emit('join', roomId);
 
+     var nick = prompt("Enter username.")
+
+
+     
      $(function () {
-     $('.ui.mini.basic.modal.start').modal('show');
-     useName = $('#addusername').val();
-     return useName;
-     console.log(useName)
-     var nick = useName;
-     socket.emit('adduser', nick);
-    });
+       socket.emit('adduser', nick);
+     });
 
   // call the server-side function 'adduser' and send one parameter (value of prompt)
       //empty messages.
