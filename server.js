88e73b4a0e7196284b03ef48855ff2982d1ee893
Your Name <you@example.com>
2019-11-04 06:47:02 -0500

    .

--- server.js
+++ server.js
@@ -475,7 +475,6 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('adduser', function(username){
     // store the username in the socket session for this client
-    util.inspect(username);
     socket.username = username;
     id = socket.id;
     
