c993af7c20ef10c52f015aea08b0c1aa5cbb5300
Your Name <you@example.com>
2019-11-12 10:40:26 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ router.get('*', function (req, res, next) {
    console.log('ok');
    console.dir(req.ip)
    console.dir(req.ips)
-   console.log(req.username)
+   console.log(req.user)
    // console.dir(req.method)
    // console.dir(req.path)
    // console.dir(req.route)
