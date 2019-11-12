54338f429b0d46229ae0916ef89dbba77c23b12e
Your Name <you@example.com>
2019-11-12 10:25:23 -0500

    .

--- public/js/app.js
+++ public/js/app.js
@@ -88,7 +88,7 @@ var date = JSON.stringify(new Date(Date.now()).toLocaleTimeString())
     // console.log("roomId : "+roomId);
     //event to get chat history on button click or as room is set.
      socket.emit('join', roomId);
-     console.log(JSON.stringify(username))
+
      console.log(JSON.stringify(user))
      console.log(JSON.stringify(name))
 
