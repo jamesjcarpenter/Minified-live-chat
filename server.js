450373b7e6449978a701c43345ea2aa15b569f60
Your Name <you@example.com>
2019-11-13 14:45:14 -0500

    .

--- server.js
+++ server.js
@@ -235,13 +235,13 @@ app.use(function(req, res, next) {
 app.use(function(req, res, next) {
   var isRoomOwner = null;
   nameRoom = req.path.split('/room/').join('')
-  if (userName == nameRoom){
+  if (userName = nameRoom){
     console.log('users room')
     console.log('path is: ' + req.path)
     console.log('nameroom is:  ' + nameRoom)
    isRoomOwner = true;
     console.log(isRoomOwner)
-  } else {
+  } if (userName != nameRoom){
     isRoomOwner = false;
     console.log('not room')
     console.log(isRoomOwner)
