623792c74e3b63a8987b6c51dbe51380fd203296
Your Name <you@example.com>
2019-11-12 19:31:31 -0500

    .

--- public/js/app.js
+++ public/js/app.js
@@ -215,7 +215,7 @@ var date = JSON.stringify(new Date(Date.now()).toLocaleTimeString())
     
 		$('#userlist').empty();
     // $('#userlist').append('<div class="list-group-item-heading"><span class="ui white text">' + 'online' + '<br>' + 'room &nbsp;#' + '' + socket.room + '</span></div><br><br>');
-		$.each(data, function(key, value) {
+		$.each(JSON.parse(data), function(key, value) {
 			$('#userlist').append('<li><span class="ui white text user">' + key  + '</span>' + '<span class="ui small white text pm"></div>' + '</a>' + '</li>');
       console.log($('.span.ui.white.text.user').val())
       $('.ui.small.white.text.pm').click( function() {
