e4a4ed0f5b6b65d06a4d4da524cd146ac02bbbff
Your Name <you@example.com>
2019-11-13 16:00:33 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -108,7 +108,7 @@ router.get('/room/:name1', function(req, res, options) {
   // res.locals.query = req.query;
   //  res.locals.url   = req.originalUrl;
   console.log('passing username as:  ' + userName)
-   res.render('index.ejs', { error: false, styleNonce: res.locals.styleNonce, name: req.params.name, chat: req.session.chat, username: userName, isRoomOwner: isRoomOwner});
+   res.render('index.ejs', { error: false, styleNonce: res.locals.styleNonce, name: req.params.name, chat: req.session.chat, username: userName});
 });
 
 router.get('/profile', function(req, res, options) {
