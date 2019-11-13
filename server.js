968f2ea44ff8217e3ca8b0328a0e98b6ce387f9f
Your Name <you@example.com>
2019-11-12 19:29:57 -0500

    .

--- server.js
+++ server.js
@@ -534,7 +534,7 @@ io.sockets.on('connection', function (socket) {
     
     socket.on('changename', function(username){
       socket.username = username;
-      io.emit('updateusers', usernames, user);
+      io.emit('updateThisUser', username, users);
     });
 
     socket.emit('serverupdatechat', 'you joined the room' + ' ' + socket.room);
