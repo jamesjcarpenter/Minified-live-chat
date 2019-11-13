21dd296d933c7312f5575f78b55936462f049fed
Your Name <you@example.com>
2019-11-13 14:09:01 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -91,22 +91,24 @@ router.get('/rooms', function(req, res, options) {
 
 
 router.get('/room/:name1', function(req, res, options) {
+  const isRoomOwner = false;
   req.user = req.isAuthenticated;
   nameRoom = req.path.split('/room/').join('')
   if (userName == nameRoom){
     console.log('users room')
     console.log('path is: ' + req.path)
     console.log('nameroom is:  ' + nameRoom)
+   isRoomOwner = true;
+    console.log(isRoomOwner)
   } else {
+    isRoomOwner = false;
     console.log('not room')
-    console.log(req.path)
-    console.log('path is: ' + req.path)
-    console.log('nameroom is:  ' + nameRoom)
+    console.log(isRoomOwner)
   }
   // res.locals.query = req.query;
   //  res.locals.url   = req.originalUrl;
   console.log('passing username as:  ' + userName)
-   res.render('index.ejs', { error: false, styleNonce: res.locals.styleNonce, name: req.params.name, chat: req.session.chat, username: userName});
+   res.render('index.ejs', { error: false, styleNonce: res.locals.styleNonce, name: req.params.name, chat: req.session.chat, username: userName, isRoomOwner: isRoomOwner});
 });
 
 router.get('/profile', function(req, res, options) {
