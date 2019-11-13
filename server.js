4b95f3ec013c61fe4a22e61448364825415e3466
Your Name <you@example.com>
2019-11-13 15:57:57 -0800

    .

--- server.js
+++ server.js
@@ -510,7 +510,7 @@ io.sockets.on('connection', function (socket) {
     
     if(isRoomOwner == true){
       console.log("yeah owner");
-      socket.username = username + '   ' + '⭐';
+      socket.username = username + '   ' + '🌟';
     } if(isRoomOwner == false){
       console.log("yeah nah")
       socket.username = username;
