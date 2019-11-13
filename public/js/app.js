53523e6f275d854551a0000c473c4655521d09c1
Your Name <you@example.com>
2019-11-13 14:21:40 -0800

    .

--- public/js/app.js
+++ public/js/app.js
@@ -194,7 +194,7 @@ var date = JSON.stringify(new Date(Date.now()).toLocaleTimeString())
 
 
 
-  $(".span.ui.white.text.user").click(function(){
+  $("#changename").click(function(){
     $('.ui.mini.basic.modal.start')
     .modal({  
       blurring: true,
