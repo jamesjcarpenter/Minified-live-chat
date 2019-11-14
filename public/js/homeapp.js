a8f4e3776541a365f3d83cd83d937d7b1efca32d
Your Name <you@example.com>
2019-11-14 07:40:25 -0800

    .

--- public/js/homeapp.js
+++ public/js/homeapp.js
@@ -95,10 +95,6 @@ window.addEventListener('load', () => {
         });
         
       
-        socket.on('private-message', (data, message) => {
-          console.log('You received a message');
-          console.log(message.data);
-          });
       //   socket.emit('adduser',
       //   $('.ui.mini.basic.modal.start')
       //   .modal({  
@@ -197,16 +193,7 @@ window.addEventListener('load', () => {
       // });
     
     // create our webrtc connection
-    socket.on('updatechat', function (username, data) {
-      $('#conversation').append('<div class="ui container"><div class="ui medium basic segment"></div></div>');
-      $('#scrollable').animate({ scrollTop: 		$('#scrollable').prop('scrollHeight')}, 300);
-      $("#data").focus();
-      // $('#usercam').empty().append($('<span class="ui text small "></span>').text(username));
-      $('#conversation').append($('<img id="useravatar" class="ui avatar image" src="/images/avatarsmall.jpg"></img><tag id="username"name="avatar"><span class="ui small text"><samp></samp></span></tag>').text(username));
-      $('#conversation').append($('<span class="ui small text" id="date"name="date"></span>').text(JSON.parse(date)));
-      $('#conversation').append($('<div class="ui left pointing label"id="message"name="data"><div id="messagedata"><p class="messaging"><span class="ui small text"></span></p></div></div>').text(data));
-      replaceUrl();
-    });
+
     
     function replaceUrl() {
       
@@ -216,21 +203,11 @@ window.addEventListener('load', () => {
         
       };
     // listener, whenever the server emits 'updaterooms', this updates the room the client is in
-    socket.on('serverupdatechat', function (server, username, data) {
-      $('#conversation').append('<div class="ui container"><div class="ui small basic segment"></div></div>');
-          $('#scrollable').animate({ scrollTop: 		$('#scrollable').prop('scrollHeight')}, 1100);
-          $('#conversation').append($('<div class="ui small grey label"id="servermessage"><span class="ui small text"></span></div>').text(server));
-          $("#roomname").empty();
-          // $("#roomname").append('<span class="ui medium text" id="roomname"><div class="ui grey label"id="roomname">Room #'+ '' + url.substr(url.lastIndexOf("=")+1) + '</span></div>');
-            });
+
   //$('#publisher').append('<h4>' + username + '</h4>');
   
     // on load of page
-  socket.on('connect', function(data) {
-      // when the client clicks SEND
-      $('#datasend').click( function() {
-        var message = $('#data').val().trim();
-        $('#data').val('');
+
         
         
         // document.getElementById("#data").value = '<div class="ui left pointing label"id="emojimsg"><img id="joyImg" src="images/images/joy.png" /></div>'
@@ -270,32 +247,7 @@ window.addEventListener('load', () => {
           return regex.replace(/([()[{*+.$^\\|?])/g, '\\$1');
         }
         
-        document.getElementById('data').oninput = function() {
-          for (var i in map) {
-            var regex = new RegExp(escapeSpecialChars(i), 'gim');
-            this.value = this.value = this.value.replace(regex, map[i]);
-          }
-        };
-        
-        // var re = new RegExp(/:\)|:-\)|:\(|:-\(|;\);-\)|:-O|8-|:P|:D|:\||:S|:\$|:@|8o\||\+o\(|\(H\)|\(C\)|\(\?\)/g); 
-        // var str = '';
-        // if (re.test(message)) {
-        //   message = str.replace(emote, str);
-        //   socket.emit('sendchat', message);
-        // };
-        // tell server to execute 'sendchat' and send along one parameter
-        // tell server to execute 'sendchat' and send along one parameter
-        socket.emit('sendchat', message);
-      });
-  
-      // when the client hits ENTER on their keyboard
-      $('#data').keypress(function(e) {
-        if(e.which == 13) {
-          $(this).blur();
-          $('#datasend').focus().click();
-        }
-      });
-    });
+
     
     
     
@@ -328,10 +280,7 @@ window.addEventListener('load', () => {
       },3500);
     }); 
     
-    socket.on('roomcount', function(rooms) {
-            $('#rmcount').empty();
-            $('#rmcount').append('' + rooms.length);
-    });
+
     
     socket.on('updateactive', function(numClients) {
       $('#globallist').empty();
@@ -363,136 +312,5 @@ window.addEventListener('load', () => {
           delete socket.usernames[socket.username];
           });
   
-      // function switchRoom(room){
-      //   socket.broadcast.emit('switchRoom', room);
-      // }
-  
-    // socket.on('onlineStack',function(stack){
-    //    $('#list').empty();
-    //    $('#list').append($('<li>').append($('<button id="ubtn" class="btn btn-danger btn-block btn-lg"></button>').text("Group").css({"font-size":"18px"})));
-    //    var totalOnline = 0;
-    //    for (var user in stack){
-    //      //setting txt1. shows users button.
-    //      if(user == username){
-    //        var txt1 = $('<button class="boxF disabled"> </button>').text(user).css({"font-size":"18px"});
-    //      }
-    //      else{
-    //        var txt1 = $('<button id="ubtn" class="btn btn-success  btn-md">').text(user).css({"font-size":"18px"});
-    //      }
-    //      //setting txt2. shows online status.
-    //      if(stack[user] == "Online"){
-    //        var txt2 = $('<span class="badge"></span>').text("*"+stack[user]).css({"float":"right","color":"#009933","font-size":"18px"});
-    //        totalOnline++;
-    // 
-    //      }
-    //      else{
-    //        var txt2 = $('<span class="badge"></span>').text(stack[user]).css({"float":"right","color":"#a6a6a6","font-size":"18px"});
-    //      }
-    //      //listing all users.
-    //      $('#list').append($('<li>').append(txt1,txt2));
-    //      $('#totalOnline').text(totalOnline);
-    //    }//end of for.
-    //    $('#scrl1').scrollTop($('#scrl1').prop("scrollHeight"));
-    //  }); //end of receiving onlineStack event.
-  
-    // create an array to hold all the usernames of the poeple in a specific room
-   // var roomusers = [];
-   //  // get all the clients in ‘room1′
-   //  // var clients = io.sockets.adapter.rooms[];
-   //  var clients;
-   //  // clients.length;
-   //  // loop through clients in ‘room1′ and add their usernames to the roomusers array
-   // for(var i = 0; i < clients.length; i++) {
-   //  roomusers[roomusers.length] = clients[i].username;
-   //  }
-   //  // broadcast to everyone in room 1 the usernames of the clients connected.
-   //  io.sockets.to(socket.room).emit('updateroomusers', roomusers);
-    
-    //index.html code
-  
-  
-    //Post Local Message
-  
-  
-    // Display Chat Interface
-  //  const showChatRoom = (room) => {
-  //    formEl.hide();
-  //    const html = chatTemplate({ room });
-  //    chatEl.html(html);
-  //    const postForm = $('form');
-  //    postForm.form({
-  //      message: 'empty',
-  //    });
-  //    $('#post-btn').on('click', () => {
-  //      const message = $('#post-message').val();
-  //      postMessage(message);
-  //    });
-  //    $('#post-message').on('keyup', (event) => {
-  //      if (event.keyCode === 13) {
-  //        const message = $('#post-message').val();
-  //        postMessage(message);
-  //      }
-  //    });
-  //  };
-  
-    // Register new Chat Room
-  
-  
-    // Join existing Chat Room
-  //  const joinRoom = (roomName) => {
-      // eslint-disable-next-line no-console
-    //  console.log(`Joining Room: ${roomName}`);
-    //  webrtc.joinRoom(roomName);
-  //    showChatRoom(roomName);
-    //  postMessage(`${username} joined chatroom`);
-  //  };
-  
-    // Receive message from remote user
-  //    webrtc.connection.on('message', (data) => {
-  //    if (data.type === 'chat') {
-  //      const message = data.payload;
-  //      messages.push(message);
-  //      updateChatMessages();
-  //   }
-  //  });
-  
-    // Room Submit Button Handler
-  
-  //   $('.jiggle .placeholder')
-  //   .transition({
-  //     animation : 'jiggle',
-  //     duration  : 1300,
-  //   });
-  // 
-  //   $('.jiggle #local-video')
-  //   .transition({
-  //     animation : 'jiggle',
-  //     duration  : 1300,
-  //   });
-  //   $('.ui.checkbox')
-  //   .checkbox()
-  // ;
-  // });
-  // $('.event .content .summary .date .ui #newmsg')
-  // .transition({
-  //   animation : 'fly in',
-  //   duration  : 1200,
-  // });
-  
-  window.onunload = function() {
-      $('#start').show();
-      $('#start').click();
-  }
-  
-  
-  // var designer = new CanvasDesigner();
-  
-  // both links are mandatory
-  // widget.html will internally use widget.js
-  // designer.widgetHtmlURL = 'https://www.webrtc-experiment.com/Canvas-Designer/widget.html'; // you can place this file anywhere
-  // designer.widgetJsURL = 'https://www.webrtc-experiment.com/Canvas-Designer/widget.js';     // you can place this file anywhere
-  
-  // <iframe> will be appended to "document.body"
-  // designer.appendTo(document.body.inner|| document.documentElement);
   });
   
