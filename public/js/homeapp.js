3d1cf629673586d45b3759dbb2d1df93244fdf20
Your Name <you@example.com>
2019-11-15 20:05:57 -0800

    .

--- public/js/homeapp.js
+++ public/js/homeapp.js
@@ -111,8 +111,8 @@ window.addEventListener('load', () => {
     
     
   
-    socket.on('updateuserhome', function(data) {
-    $('#userlist').append('<div class="list-group-item-heading"><span class="ui white text">' + 'online' + '<br>' + 'room &nbsp;#' + '' + clients.length + '</span></div><br><br>');   
+    socket.on('updateuserhome', function(room) {
+    $('#userlist').append('<div class="list-group-item-heading"><span class="ui white text">' + 'online' + '<br>' + 'room &nbsp;#' + '' + room.length + '</span></div><br><br>');   
     // $.each(data, function(key, value) {
           //     $('#userlist').append('<span class="ui white text">' + key  + '</span>' + '</a>' + '<br>');
           //     console.log('key for usershome is:  ' + key)
