abf82a3b13876ddf9d5af23407749adbad1855b9
Your Name <you@example.com>
2019-11-15 20:13:36 -0800

    .

--- server.js
+++ server.js
@@ -543,7 +543,7 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('findUser', function(socket){
     var userNameOfUserToFind;
-     User.findOne({ username : userName }).exec(function(err,res) {
+     User.findOne({ userNameOfUserToFind : socket }).exec(function(err,res) {
        console.log(res);
         // io.to(res.socketId).emit('my message', msg);
       })
