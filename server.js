b2da7a5174fafb242734d24c0d60a5d17dca1e99
Your Name <you@example.com>
2019-11-15 18:18:59 -0800

    .

--- server.js
+++ server.js
@@ -205,7 +205,7 @@ const SocketAntiSpam  = require('socket-anti-spam');
 
 const socketAntiSpam = new SocketAntiSpam({
   banTime:            1,         // Ban time in minutes
-  kickThreshold:      44,          // User gets kicked after this many spam score
+  kickThreshold:      33,          // User gets kicked after this many spam score
   kickTimesBeforeBan: 3,          // User gets banned after this many kicks
   banning:            true,       // Uses temp IP banning after kickTimesBeforeBan
   io:                 io,  // Bind the socket.io variable
